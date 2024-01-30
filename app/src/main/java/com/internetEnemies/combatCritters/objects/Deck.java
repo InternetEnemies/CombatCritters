cbc5893f8202950d065d91dba144b76116c5381a
VanJackal <v@njackal.com>
2024-01-30 10:18:06 -0600

    Deck Skeleton

---
+++ app/src/main/java/com/internetEnemies/combatCritters/objects/Deck.java
@@ -0,0 +1,14 @@
+package com.internetEnemies.combatCritters.objects;
+
+import java.util.List;
+
+public class Deck {
+    private final int id;
+    private final String name;
+    private final List<Card> cards;
+    public Deck(int id, String name, List<Card> cards) {
+        this.id = id;
+        this.name = name;
+        this.cards = cards;
+    }
+}
