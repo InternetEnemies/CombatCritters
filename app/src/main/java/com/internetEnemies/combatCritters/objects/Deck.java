2541b7a002b280c483e11302a1e5e44e7203b8b5
jackal <v@njackal.com>
2024-01-30 21:40:15 -0600

    updated Deck DSO to remove the list of cards (should be requested from the database through IDeck)

--- app/src/main/java/com/internetEnemies/combatCritters/objects/Deck.java
+++ app/src/main/java/com/internetEnemies/combatCritters/objects/Deck.java
@@ -5,11 +5,9 @@ import java.util.List;
 public class Deck {
     private final int id;
     private final String name;
-    private final List<Card> cards;
-    public Deck(int id, String name, List<Card> cards) {
+    public Deck(int id, String name) {
         this.id = id;
         this.name = name;
-        this.cards = cards;
     }
     public int getId() {
         return id;
@@ -18,9 +16,4 @@ public class Deck {
     public String getName() {
         return name;
     }
-
-    public List<Card> getCards() {
-        return cards;
-    }
-
 }
