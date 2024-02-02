2582a4aa7a75856ec9b81411032732399b7c00d0
Hugo Ng <ym7ym7228@gmail.com>
2024-02-02 17:58:26 -0600

    update: constructor

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/DeckBuilder.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/DeckBuilder.java
@@ -9,11 +9,16 @@
 
 package com.internetEnemies.combatCritters.Logic;
 
+import com.internetEnemies.combatCritters.data.DeckInventoryStub;
 import com.internetEnemies.combatCritters.data.IDeckInventory;
 
 public class DeckBuilder implements IDeckBuilder{
 
     private IDeckInventory deckInventory;
+
+    public DeckBuilder(){
+        deckInventory = new DeckInventoryStub();
+    }
 
     @Override
     public boolean createNewDeck(String name) {
