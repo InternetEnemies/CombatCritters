5b2368616d50cf280fec1ada1857bd5e916b2fab
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-02-01 22:21:40 -0600

    created: DeckBuilder.java & IDeckBuilder.java

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/DeckBuilder.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/DeckBuilder.java
@@ -1,5 +1,18 @@
+/**
+ * DeckBuilder.java
+ * COMP 3350 A02
+ * Project      combat critters
+ * @created     01-February-2024
+ *
+ * PURPOSE:     perform an implementation of IDeckBuilder
+ */
+
 package com.internetEnemies.combatCritters.Logic;
 
-public interface DeckBuilder {
+public class DeckBuilder implements IDeckBuilder{
 
+    @Override
+    public void createNewDeck() {
+
+    }
 }
---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/IDeckBuilder.java
@@ -0,0 +1,18 @@
+/**
+ * IDeckBuilder.java
+ * COMP 3350 A02
+ * Project      combat critters
+ * @created     01-February-2024
+ *
+ * PURPOSE:     An interface for Deck builder
+ */
+
+package com.internetEnemies.combatCritters.Logic;
+
+
+
+public interface IDeckBuilder {
+
+    void createNewDeck();
+
+}
