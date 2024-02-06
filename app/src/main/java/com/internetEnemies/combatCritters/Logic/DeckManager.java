6552714283bfb2e19b35ef4343a6509b3710082c
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-02-06 07:31:08 -0600

    update: DeckManager: basic interface

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/DeckManager.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/DeckManager.java
@@ -1,6 +1,8 @@
 package com.internetEnemies.combatCritters.Logic;
 
 import com.internetEnemies.combatCritters.data.Database;
+import com.internetEnemies.combatCritters.data.IDeck;
+import com.internetEnemies.combatCritters.data.IDeckInventory;
 import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.DeckDetails;
 
@@ -8,7 +10,70 @@ import java.util.List;
 import java.util.Map;
 
 public class DeckManager{
-    private Database db;
-    private DeckBuilder builder;
 
+    private IDeckInventory deckInventory;
+    public DeckManager(){
+        deckInventory = Database.getInstance().getDeckInventory();
+    }
+
+    public DeckManager(IDeckInventory deckInventory){
+        //testing constructor
+        this.deckInventory = deckInventory;
+    }
+
+    /**
+     * Creates a new Deck into deckInventory
+     * @param name name of the deck to be created
+     * @return DeckDetails of the deck if the deck successfully created,
+     *         null if the an error appear and the deck is not created
+     */
+    public DeckDetails createDeck(String name){
+        return null;
+    }
+
+    /**
+     * Delete the Deck with given DeckDetails
+     * @param deckInfo the DeckDetails of the deck wanted to delete
+     * @return true if the deck successfully deleted, false if the an error appear and the deck still remains
+     */
+    public boolean deleteDeck(DeckDetails deckInfo){
+        return true;
+    }
+
+    /**
+     * Return a DeckBuilder object for modifying the deck
+     * @param deckInfo the DeckDetails of the deck wanted to modify with
+     * @return a DeckBuilder object of the deck,
+     *         null if the deck is no longer in the DeckInventory
+     */
+    DeckBuilder getBuilder(DeckDetails deckInfo){
+        return null;
+    }
+
+    /**
+     * get the list of the decks for further purpose
+     * @return A list of DeckDetails of current stored decks,
+     *         null if nothing inside the deckInventory
+     */
+    public List<DeckDetails> getDecks(){
+        return null;
+    }
+
+    /**
+     * Get the deck in the deck inventory, use after validateDeck()
+     * @param deckInfo the DeckDetails of the deck
+     * @return the Deck
+     */
+    private IDeck getDeck(DeckDetails deckInfo){
+        try{
+            IDeck deck = null;
+            for (IDeck toCompare : deckInventory) {
+                if (toCompare.getInfo() == deckInfo) {
+                    deck = toCompare;
+                    break;
+                }
+            }
+            return deck;
+        }catch (Exception e){return null;}
+    }
 }
--- app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/DeckManagerTest.java
+++ app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/DeckManagerTest.java
@@ -1,249 +1,123 @@
-/**
- * DeckMangerTest.java
- * COMP 3350 A02
- * Project      combat critters
- * @created     05-February-2024
- *
- * PURPOSE:     Unit Test for DeckManager, since DeckBuilder and DeckHandler are protected classes,
- *              this unit test will be an integrated test for these three classes
- */
-
 package com.internetEnemies.combatCritters.LogicUnitTests;
 
 import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-import com.internetEnemies.combatCritters.Logic.IDeckManager;
 import com.internetEnemies.combatCritters.Logic.DeckManager;
-import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.DeckDetails;
-import com.internetEnemies.combatCritters.objects.ItemCard;
 
 public class DeckManagerTest {
-    private IDeckManager deckManager;
 
-    private final static Card[] cards = {
-            new ItemCard(1,"","",1, Card.Rarity.RARE,1),
-            new ItemCard(2,"","",1, Card.Rarity.RARE,1),
-            new ItemCard(3,"","",1, Card.Rarity.RARE,1),
-            new ItemCard(4,"","",1, Card.Rarity.RARE,1)
-    };
+    private DeckManager deckManager;
 
     @Before
-    public void setup() {deckManager = new DeckManager();}
+    public void setup() {
+        deckManager = new DeckManager();
+    }
 
     @Test
-    public void testConstructor(){
+    public void testConstructor() {
         assertNotNull(deckManager);
         assertTrue(deckManager instanceof DeckManager);
     }
 
     @Test
-    public void testHandlerCreateDeck(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
+    public void testCreateDeck() {
+        DeckDetails test1Info = deckManager.createDeck("test1");
         assertNotNull(test1Info);
-        assertEquals("test1",test1Info.getName());
-        assertEquals(0,test1Info.getId());
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
+        assertEquals("test1", test1Info.getName());
+        assertEquals(0, test1Info.getId());
+        assertEquals(deckManager.getInventory().getDeck(0).getInfo(), test1Info);
     }
 
     @Test
-    public void testHandlerCreateMultipleDecks(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        DeckDetails test2Info = deckManager.handlerCreateDeck("test2");
-        DeckDetails test3Info = deckManager.handlerCreateDeck("test3");
-        assertNotNull(test1Info);
-        assertNotNull(test2Info);
-        assertNotNull(test3Info);
-        assertEquals("test1",test1Info.getName());
-        assertEquals("test2",test2Info.getName());
-        assertEquals("test3",test3Info.getName());
-        assertEquals(0,test1Info.getId());
-        assertEquals(1,test2Info.getId());
-        assertEquals(2,test3Info.getId());
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
-        assertTrue(deckManager.handlerGetDecks().contains(test2Info));
-        assertTrue(deckManager.handlerGetDecks().contains(test3Info));
-    }
-
-    @Test
-    public void testHandlerCreateDeckFailure(){
-        assertNull(deckManager.handlerCreateDeck(null));
-    }
-
-    @Test
-    public void testHandlerDeleteDeck(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.handlerDeleteDeck(test1Info));
-        test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
-    }
-
-    @Test
-    public void testHandlerDeleteMultipleDecks(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        DeckDetails test2Info = deckManager.handlerCreateDeck("test2");
-        DeckDetails test3Info = deckManager.handlerCreateDeck("test3");
+    public void testCreateMultipleDecks() {
+        DeckDetails test1Info = deckManager.createDeck("test1");
+        DeckDetails test2Info = deckManager.createDeck("test2");
+        DeckDetails test3Info = deckManager.createDeck("test3");
         assertNotNull(test1Info);
         assertNotNull(test2Info);
         assertNotNull(test3Info);
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
-        assertTrue(deckManager.handlerGetDecks().contains(test2Info));
-        assertTrue(deckManager.handlerGetDecks().contains(test3Info));
-        assertTrue(deckManager.handlerDeleteDeck(test1Info));
-        assertTrue(deckManager.handlerDeleteDeck(test2Info));
-        assertTrue(deckManager.handlerDeleteDeck(test3Info));
+        assertEquals("test1", test1Info.getName());
+        assertEquals("test2", test2Info.getName());
+        assertEquals("test3", test3Info.getName());
+        assertEquals(0, test1Info.getId());
+        assertEquals(1, test2Info.getId());
+        assertEquals(2, test3Info.getId());
+        assertEquals(deckManager.getInventory().getDeck(0).getInfo(), test1Info);
+        assertEquals(deckManager.getInventory().getDeck(1).getInfo(), test2Info);
+        assertEquals(deckManager.getInventory().getDeck(2).getInfo(), test3Info);
     }
 
     @Test
-    public void testHandlerDeleteDeckFailure(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertNotNull(test1Info);
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
-        assertTrue(deckManager.handlerDeleteDeck(test1Info));
-        assertFalse(deckManager.handlerDeleteDeck(test1Info));
-        test1Info = deckManager.handlerCreateDeck("test1");
-        assertNotNull(test1Info);
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
-        assertFalse(deckManager.handlerDeleteDeck(null));
-        assertTrue(deckManager.handlerDeleteDeck(test1Info));
+    public void testCreateDeckFailure() {
+        assertNull(deckManager.createDeck(null));
     }
 
     @Test
-    public void testHandlerGetDecks(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        DeckDetails test2Info = deckManager.handlerCreateDeck("test2");
-        DeckDetails test3Info = deckManager.handlerCreateDeck("test3");
+    public void testDeleteDeck() {
+        DeckDetails test1Info = deckManager.createDeck("test1");
+        assertTrue(deckManager.deleteDeck(test1Info));
+        test1Info = deckManager.createDeck("test1");
+        assertTrue(deckManager.deleteDeck(test1Info));
+    }
+
+    @Test
+    public void testDeleteMultipleDecks() {
+        DeckDetails test1Info = deckManager.createDeck("test1");
+        DeckDetails test2Info = deckManager.createDeck("test2");
+        DeckDetails test3Info = deckManager.createDeck("test3");
         assertNotNull(test1Info);
         assertNotNull(test2Info);
         assertNotNull(test3Info);
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
-        assertTrue(deckManager.handlerGetDecks().contains(test2Info));
-        assertTrue(deckManager.handlerGetDecks().contains(test3Info));
+        assertEquals(deckManager.getInventory().getDeck(0).getInfo(), test1Info);
+        assertEquals(deckManager.getInventory().getDeck(1).getInfo(), test2Info);
+        assertEquals(deckManager.getInventory().getDeck(2).getInfo(), test3Info);
+        assertTrue(deckManager.deleteDeck(test1Info));
+        assertTrue(deckManager.deleteDeck(test2Info));
+        assertTrue(deckManager.deleteDeck(test3Info));
     }
 
     @Test
-    public void testHandlerGetDeletedDecks(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
+    public void testDeleteDeckFailure() {
+        DeckDetails test1Info = deckManager.createDeck("test1");
         assertNotNull(test1Info);
-        assertTrue(deckManager.handlerGetDecks().contains(test1Info));
-        assertTrue(deckManager.handlerDeleteDeck(test1Info));
-        assertFalse(deckManager.handlerGetDecks().contains(test1Info));
+        assertEquals(deckManager.getInventory().getDeck(0).getInfo(), test1Info);
+        assertTrue(deckManager.deleteDeck(test1Info));
+        assertFalse(deckManager.deleteDeck(test1Info));
+        test1Info = deckManager.createDeck("test1");
+        assertNotNull(test1Info);
+        assertEquals(deckManager.getInventory().getDeck(1).getInfo(), test1Info);
+        assertFalse(deckManager.deleteDeck(null));
+        assertTrue(deckManager.deleteDeck(test1Info));
     }
 
     @Test
-    public void testBuilderSearchingInventory(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        //true cases
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-        //false cases
-        deckManager.handlerDeleteDeck(test1Info);
-        assertFalse(deckManager.builderAddCard(cards[0],test1Info));
-        assertFalse(deckManager.builderRemoveCard(cards[0],test1Info));
+    public void testGetDecks() {
+        DeckDetails test1Info = deckManager.createDeck("test1");
+        DeckDetails test2Info = deckManager.createDeck("test2");
+        DeckDetails test3Info = deckManager.createDeck("test3");
+        assertNotNull(test1Info);
+        assertNotNull(test2Info);
+        assertNotNull(test3Info);
+        assertEquals(deckManager.getInventory().getDeck(0).getInfo(), test1Info);
+        assertEquals(deckManager.getInventory().getDeck(1).getInfo(), test2Info);
+        assertEquals(deckManager.getInventory().getDeck(2).getInfo(), test3Info);
+        assertTrue(deckManager.getDecks().contains(test1Info));
+        assertTrue(deckManager.getDecks().contains(test2Info));
+        assertTrue(deckManager.getDecks().contains(test3Info));
     }
 
     @Test
-    public void testBuilderAddCard(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
+    public void testGetDeletedDecks() {
+        DeckDetails test1Info = deckManager.createDeck("test1");
+        assertNotNull(test1Info);
+        assertEquals(deckManager.getInventory().getDeck(0).getInfo(), test1Info);
+        assertTrue(deckManager.getDecks().contains(test1Info));
+        assertTrue(deckManager.deleteDeck(test1Info));
+        assertFalse(deckManager.getDecks().contains(test1Info));
     }
 
-    @Test
-    public void testBuilderAddMultipleCards(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertEquals(2,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
-    }
-
-    @Test
-    public void testBuilderAddMultipleTypesCard(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[1],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[2],test1Info));
-        assertEquals(2,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[1]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[2]));
-    }
-
-    @Test
-    public void testBuilderAddCardFailure(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertFalse(deckManager.builderAddCard(null,test1Info));
-        assertFalse(deckManager.builderAddCard(cards[0],null));
-    }
-
-    @Test
-    public void testBuilderRemoveCard(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-    }
-
-    @Test
-    public void testBuilderRemoveMultipleCards(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertEquals(4,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-        assertNull(deckManager.builderGetCards(test1Info).get(cards[0]));
-    }
-
-    @Test
-    public void testBuilderRemoveMultipleTypesCard(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[1],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[2],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[3],test1Info));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[1]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[2]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[3]));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-        assertTrue(deckManager.builderRemoveCard(cards[1],test1Info));
-        assertTrue(deckManager.builderRemoveCard(cards[2],test1Info));
-        assertTrue(deckManager.builderRemoveCard(cards[3],test1Info));
-        assertNull(deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertNull(deckManager.builderGetCards(test1Info).get(cards[1]));
-        assertNull(deckManager.builderGetCards(test1Info).get(cards[2]));
-        assertNull(deckManager.builderGetCards(test1Info).get(cards[3]));
-    }
-
-    @Test
-    public void testBuilderRemoveCardFailure(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertTrue(deckManager.builderRemoveCard(cards[0],test1Info));
-        assertNull(deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertFalse(deckManager.builderRemoveCard(cards[0],test1Info));
-        assertFalse(deckManager.builderRemoveCard(null,test1Info));
-        assertFalse(deckManager.builderRemoveCard(cards[0],null));
-    }
-
-    @Test
-    public void testBuilderGetCards(){
-        DeckDetails test1Info = deckManager.handlerCreateDeck("test1");
-        assertTrue(deckManager.builderAddCard(cards[0],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[1],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[2],test1Info));
-        assertTrue(deckManager.builderAddCard(cards[3],test1Info));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[0]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[1]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[2]));
-        assertEquals(1,(int)deckManager.builderGetCards(test1Info).get(cards[3]));
-    }
 }
