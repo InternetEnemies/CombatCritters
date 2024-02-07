07aa0ddb27a5ba1cef36b3aba16a4a94dcf30a23
VanJackal <v@njackal.com>
2024-02-07 10:07:01 -0600

    added getAll to CardCatalog

--- app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/CardCatalogTest.java
+++ app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/CardCatalogTest.java
@@ -14,10 +14,12 @@ import com.internetEnemies.combatCritters.data.Registry;
 import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.ItemCard;
 
+import java.util.Map;
+
 public class CardCatalogTest {
     CardCatalog catalog;
     ICardInventory inventory;
-    IRegistry<Card> cards;
+    Registry<Card> cards;
     @Before
     public void setup() {
         inventory = new CardInventoryStub();
@@ -36,5 +38,18 @@ public class CardCatalogTest {
         Card card = new ItemCard(0,"","",1, Card.Rarity.COMMON,1);
         inventory.addCard(card);
         assertEquals((int)catalog.getOwned().get(card),1);
+    }
+
+    @Test
+    public void getAll() {
+        Card card = new ItemCard(0,"","",1, Card.Rarity.COMMON,1);
+        inventory.addCard(card);
+        cards.add(card);
+        cards.add(new ItemCard(1,"","",1, Card.Rarity.COMMON,1));
+
+        Map<Card,Integer> counts = catalog.getAll();
+
+        assertEquals(1,(int)counts.get(cards.getSingle(0)));
+        assertEquals(0,(int)counts.get(cards.getSingle(1)));
     }
 }
