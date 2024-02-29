a3fa0d5db65fb74b661a29086147f81124f92e63
LucasBerzuk <berzukl@myumanitoba.ca>
2024-02-29 12:59:01 -0600

    files created for HSQLDB

---
+++ app/src/main/java/com/internetEnemies/combatCritters/data/CombatCrittersHSQLDBPersistence.java
@@ -0,0 +1,6 @@
+package com.internetEnemies.combatCritters.data;
+
+import com.internetEnemies.combatCritters.data.hsqldb.CombatCrittersHSQLDB;
+
+public interface CombatCrittersHSQLDBPersistence {
+}
---
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/CombatCrittersHSQLDB.java
@@ -0,0 +1,6 @@
+package com.internetEnemies.combatCritters.data.hsqldb;
+
+import com.internetEnemies.combatCritters.data.CombatCrittersHSQLDBPersistence;
+
+public class CombatCrittersHSQLDB implements CombatCrittersHSQLDBPersistence{
+}
