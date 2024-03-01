a92580615212f10f32b3dec76913e80d82898149
chtadena <149117831+chtadena@users.noreply.github.com>
2024-03-01 12:39:56 -0600

    added interface for TransactionVisitor

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/ITransactionVisitor.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/ITransactionVisitor.java
@@ -5,9 +5,19 @@ import com.internetEnemies.combatCritters.objects.TransactionCurrency;
 import com.internetEnemies.combatCritters.objects.TransactionPack;
 
 public interface ITransactionVisitor {
+    /**
+     * Performs the necessary transaction operations for a TransactionItem holding a card(s).
+     *
+     */
     void visitCard(TransactionCard card);
-
+    /**
+     * Performs the necessary transaction operations for a TransactionItem holding a Pack(s).
+     *
+     */
     void visitPack(TransactionPack pack);
-
+    /**
+     * Performs the necessary transaction operations for a TransactionItem holding Currency.
+     *
+     */
     void visitCurrency(TransactionCurrency currency);
 }
--- app/src/main/java/com/internetEnemies/combatCritters/objects/TransactionItem.java
+++ app/src/main/java/com/internetEnemies/combatCritters/objects/TransactionItem.java
@@ -12,10 +12,20 @@ public abstract class TransactionItem<T>  {
     public int getQuantity() {
         return quantity;
     }
-
-
+    /**
+     * Validates that the item(s) exists within the player's inventory via the visitor interface.
+     * @return an instantiated Transaction object.
+     *
+     */
     public abstract void acceptValidate(ITransactionVisitor visitor);
-
+    /**
+     * Performs the proper logic to add to a respective inventory by type via the visitor interface.
+     *
+     */
     public abstract void acceptAdd(ITransactionVisitor visitor);
+    /**
+     * Performs the proper logic to remove from a respective inventory by type via the visitor interface.
+     *
+     */
     public abstract void acceptRemove(ITransactionVisitor visitor);
 }
