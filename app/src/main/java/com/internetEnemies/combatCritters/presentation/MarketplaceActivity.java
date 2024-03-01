3aafc4b33a9edbaf1a69c6830281049f63148c47
Kevin Breckman <kevinbreckman@gmail.com>
2024-03-01 11:50:31 -0600

    Add button to main menu in MarketplaceActivity

--- app/src/main/java/com/internetEnemies/combatCritters/presentation/MarketplaceActivity.java
+++ app/src/main/java/com/internetEnemies/combatCritters/presentation/MarketplaceActivity.java
@@ -1,4 +1,28 @@
 package com.internetEnemies.combatCritters.presentation;
 
-public class MarketplaceActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.internetEnemies.combatCritters.databinding.ActivityMarketplaceBinding;
+
+public class MarketplaceActivity extends AppCompatActivity {
+    private ActivityMarketplaceBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMarketplaceBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        onCreateSetup();
+    }
+
+    private void onCreateSetup() {
+        binding.buttonMainMenu.setOnClickListener((View buttonView) -> {
+            Intent intent = new Intent(MarketplaceActivity.this, MainMenuActivity.class);
+            startActivity(intent);
+        });
+    }
 }
