800a2723ccf52da34f28d1dfdc8d719f6bba2f10
Kevin Breckman <kevinbreckman@gmail.com>
2024-03-01 10:42:34 -0600

    Create MainMenuActivity

---
+++ app/src/main/java/com/internetEnemies/combatCritters/presentation/MainMenuActivity.java
@@ -0,0 +1,38 @@
+package com.internetEnemies.combatCritters.presentation;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.internetEnemies.combatCritters.R;
+import com.internetEnemies.combatCritters.databinding.ActivityDeckBuilderBinding;
+import com.internetEnemies.combatCritters.databinding.ActivityMainMenuBinding;
+
+public class MainMenuActivity extends AppCompatActivity {
+    private ActivityMainMenuBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        onCreateSetup();
+    }
+
+    private void onCreateSetup() {
+
+        binding.buttonToDeckBuilder.setOnClickListener((View buttonView) -> {
+            Intent intent = new Intent(MainMenuActivity.this, DeckBuilderActivity.class);
+            startActivity(intent);
+        });
+
+        binding.buttonToPacks.setOnClickListener((View buttonView) -> {
+            Intent intent = new Intent(MainMenuActivity.this, PackOpeningActivity.class);
+            startActivity(intent);
+        });
+
+    }
+}
