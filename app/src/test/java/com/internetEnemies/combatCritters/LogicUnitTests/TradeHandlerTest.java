0b56c697424f1cf47b1a8e6d7591790f740d0d61
Hugo Ng <ym7ym7228@gmail.com>
2024-03-02 14:42:23 -0600

    update: TradeHandlerTest: selectOffer(): testOffersContent

--- app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/TradeHandlerTest.java
+++ app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/TradeHandlerTest.java
@@ -16,6 +16,9 @@ import static org.junit.Assert.*;
 import com.internetEnemies.combatCritters.Logic.ITradesHandler;
 import com.internetEnemies.combatCritters.Logic.TradesHandler;
 import com.internetEnemies.combatCritters.data.IRegistry;
+import com.internetEnemies.combatCritters.objects.Transaction;
+
+import java.util.List;
 
 public class TradeHandlerTest {
 
@@ -35,5 +38,24 @@ public class TradeHandlerTest {
     @Test
     public void testTradesHandlerConstructor(){
         assert tradesHandler != null;
+    }
+
+    @Test
+    public void testGetOffers(){
+        int numOfGetting = tradesHandler.getOffers().size();
+        assertEquals(numOfOffers,numOfGetting);
+    }
+
+    @Test void testOffersContent(){
+        List<Transaction> tempList = tradesHandler.getOffers();
+        List<Transaction> tempRegList = tradeRegistry.getAll();
+        for(Transaction i: tempList){
+            assert(tempRegList.contains(i));
+        }
+    }
+
+    @Test
+    public void testSelectOffer(){
+
     }
 }
