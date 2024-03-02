76bba25779fb1149d8d20db22611b43ea5ea9b33
Hugo Ng <ym7ym7228@gmail.com>
2024-03-02 12:40:46 -0600

    update: TradesHandler: constructor missing: null constructor

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/TradesHandler.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/TradesHandler.java
@@ -9,22 +9,27 @@
 
 package com.internetEnemies.combatCritters.Logic;
 
+import com.internetEnemies.combatCritters.data.IRegistry;
 import com.internetEnemies.combatCritters.objects.Transaction;
 
 import java.util.List;
 
 public class TradesHandler implements ITradesHandler{
+    private final IRegistry<Transaction> tradeRegistry;
 
-    public TradesHandler(){
-
+    public TradesHandler(IRegistry<Transaction> tradeRegistry){
+        this.tradeRegistry = tradeRegistry;
     }
+
+    //TODO: null constructor
+    // should adapt implementation from TradeDB (not sure the name), like in PackCatalog
 
     /**
      * @return A list of fetched offers fetched from TradeRegistry
      */
     @Override
     public List<Transaction> getOffers() {
-        return null;
+
     }
 
     /**
