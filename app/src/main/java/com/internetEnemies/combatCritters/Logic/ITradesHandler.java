da9d200f2884f2ae02ec7711c6c545321453ccb9
Hugo Ng <ym7ym7228@gmail.com>
2024-03-02 12:11:22 -0600

    update: ITradesHandler: interfaces

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/ITradesHandler.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/ITradesHandler.java
@@ -9,5 +9,21 @@
 
 package com.internetEnemies.combatCritters.Logic;
 
+import com.internetEnemies.combatCritters.objects.Transaction;
+
+import java.util.List;
+
 public interface ITradesHandler {
+
+    /**
+     *
+     * @return A list of offers fetched from
+     */
+    List<Transaction> getOffers();
+
+    /**
+     * pass the chosen offer to transaction Handler
+     * @param offer The offer user want to accept
+     */
+    void chooseOffer(Transaction offer);
 }
