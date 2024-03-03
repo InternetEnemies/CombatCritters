44fcf9fcb805ee0547bbcd0e3c90133deea3571f
Hugo Ng <ym7ym7228@gmail.com>
2024-03-03 11:38:07 -0600

    create: IMarketHandler

---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/IMarketHandler.java
@@ -0,0 +1,13 @@
+/**
+ * IMarketHandler.java
+ * COMP 3350 A02
+ * @Project      combat critters
+ * @created      03-March-2024
+ *
+ * @PURPOSE:     Handle the the MarketDB offers and pass the offer to TransactionHandler
+ */
+
+package com.internetEnemies.combatCritters.Logic;
+
+public interface IMarketHandler {
+}
