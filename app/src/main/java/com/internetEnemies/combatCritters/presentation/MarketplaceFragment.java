7a86b062297bdf9116fe5523d88c303a8e53cfb2
Kevin Breckman <kevinbreckman@gmail.com>
2024-03-03 10:28:46 -0600

    move tab layout into marketplace fragment

---
+++ app/src/main/java/com/internetEnemies/combatCritters/presentation/MarketplaceFragment.java
@@ -0,0 +1,39 @@
+package com.internetEnemies.combatCritters.presentation;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.internetEnemies.combatCritters.R;
+import com.internetEnemies.combatCritters.objects.Card;
+import com.internetEnemies.combatCritters.presentation.renderable.CardRenderer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarketplaceFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_marketplace, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+
+    }
+
+}
