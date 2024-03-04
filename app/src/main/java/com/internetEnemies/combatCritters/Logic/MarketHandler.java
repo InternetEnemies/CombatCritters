a0850faf7562fcbf3c39247d1d41261b528c1551
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-03 22:19:40 -0600

    create: MarketHandler

---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/MarketHandler.java
@@ -0,0 +1,15 @@
+/**
+ * IMarketHandler.java
+ * COMP 3350 A02
+ * @Project      combat critters
+ * @created      03-March-2024
+ *
+ * @PURPOSE:     implementation for IMarketHandler
+ *               Provide UI for MarketPlace UI
+ *               Fetch offers from MarketDB
+ */
+
+package com.internetEnemies.combatCritters.Logic;
+
+public class MarketHandler implements IMarketHandler{
+}
