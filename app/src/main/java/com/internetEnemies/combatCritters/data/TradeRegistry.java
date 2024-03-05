4b4c84c9702954cb56ab969f7878ad2b20a23a0b
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-04 20:25:04 -0600

    create: TradeRegistry

---
+++ app/src/main/java/com/internetEnemies/combatCritters/data/TradeRegistry.java
@@ -0,0 +1,15 @@
+/**
+ * TradeRegistry.java
+ * COMP 3350 A02
+ * @Project      combat critters
+ * @created      04-March-2024
+ *
+ * @PURPOSE:     implements IRegistry
+ *               provide TradeTransaction to TradesHandler
+ *               Manage a list of TradeTransaction
+ */
+
+package com.internetEnemies.combatCritters.data;
+
+public class TradeRegistry {
+}
