13ee68d3cd5d730e6e32aaa21724ee2d83531420
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-04 21:04:48 -0600

    update: TradeRegistry: implementation

--- app/src/main/java/com/internetEnemies/combatCritters/data/TradeRegistry.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/TradeRegistry.java
@@ -11,5 +11,55 @@
 
 package com.internetEnemies.combatCritters.data;
 
-public class TradeRegistry {
+import com.internetEnemies.combatCritters.objects.Transaction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+//TODO should use TradeTransaction instead
+public class TradeRegistry implements IRegistry<Transaction>{
+    private final List<Transaction> offers;
+
+    public TradeRegistry(List<Transaction> start){
+        this();
+        offers.addAll(start);
+    }
+
+    public TradeRegistry(){
+        offers = new ArrayList<Transaction>();
+    }
+
+    @Override
+    public Transaction getSingle(int id) {
+        return offers.get(id);
+    }
+
+    @Override
+    public List getAll() {
+        return Collections.unmodifiableList(new ArrayList<>(offers));
+    }
+
+    @Override
+    public List getListOf(List<Integer> ids) {
+        List<Transaction> result = new ArrayList<>();
+        for(int id : ids) {
+            result.add(offers.get(id));
+        }
+        return result;
+    }
+
+    /**
+     *  add an offer into MarketDB
+     * @param offer a TradeTransaction to add
+     */
+    public void add(Transaction offer){
+        assert offer != null;
+        if(offers.contains(offer)){
+            throw new UnsupportedOperationException("Trade offer already in TradeRegistry");
+        }
+        offers.add(offer);
+    }
+
 }
