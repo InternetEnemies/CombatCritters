e1b4ccafa18f2e4412752ec4e91fa55c4bdd4be8
Hugo Ng <ym7ym7228@gmail.com>
2024-03-05 20:20:21 -0600

    create: OffersDatabase: basic architecture

---
+++ app/src/main/java/com/internetEnemies/combatCritters/data/OffersDatabase.java
@@ -0,0 +1,36 @@
+package com.internetEnemies.combatCritters.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OffersDatabase {
+    private static OffersDatabase INSTANCE;
+
+    private final TradeRegistry tradesDB;
+    private final MarketDB marketDB;
+
+    public static OffersDatabase getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new OffersDatabase();
+        }
+        return INSTANCE;
+    }
+
+    public TradeRegistry getTradesDB() {
+        return tradesDB;
+    }
+
+    public MarketDB getMarketDB() {
+        return marketDB;
+    }
+
+    private OffersDatabase() {
+        List<TradeTransaction> testTradeOffer = new ArrayList<TradeTransaction>();
+        List<MarketTransaction> testMarketOffer = new ArrayList<MarketTransaction>();
+
+        // make the instance
+
+        tradesDB = new TradeRegistry(testTradeOffer);
+        marketDB = new MarketDB(testMarketOffer);
+    }
+}
