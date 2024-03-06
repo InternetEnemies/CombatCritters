20c120883b51afd3dcd150b85b9b93ca5709babe
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-05 20:53:51 -0600

    update: OffersDatabase: import PackCardDatabase

--- app/src/main/java/com/internetEnemies/combatCritters/data/OffersDatabase.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/OffersDatabase.java
@@ -5,11 +5,14 @@
  * @created     2024-03-04
  *
  * @PURPOSE:    A fake Database for offers in marketDB and TradeRegistry
+ *              relies on PackCardDatabase
  */
 
 package com.internetEnemies.combatCritters.data;
 
+import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.MarketTransaction;
+import com.internetEnemies.combatCritters.objects.Pack;
 import com.internetEnemies.combatCritters.objects.TradeTransaction;
 
 import java.util.ArrayList;
@@ -37,10 +40,14 @@ public class OffersDatabase {
     }
 
     private OffersDatabase() {
+        IRegistry<Pack> packDB = PackCardDatabase.getInstance().getPackDB();
+        IRegistry<Card> cardDB = PackCardDatabase.getInstance().getCardDB();
         List<TradeTransaction> testTradeOffer = new ArrayList<TradeTransaction>();
         List<MarketTransaction> testMarketOffer = new ArrayList<MarketTransaction>();
 
         // make the instance
+        // 10 Market offers
+        // 10 Trade offers
 
 
         tradesDB = new TradeRegistry(testTradeOffer);
