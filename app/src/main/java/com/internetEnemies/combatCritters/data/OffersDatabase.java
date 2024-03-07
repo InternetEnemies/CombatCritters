9c7d93d535741b5a420ecbfa5ca3786c6026d48e
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-06 22:40:31 -0600

    update: OffersDatabase: instances

--- app/src/main/java/com/internetEnemies/combatCritters/data/OffersDatabase.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/OffersDatabase.java
@@ -10,15 +10,13 @@
 
 package com.internetEnemies.combatCritters.data;
 
-import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.Currency;
+import com.internetEnemies.combatCritters.objects.IItem;
 import com.internetEnemies.combatCritters.objects.IMarketTransactionBuilder;
-import com.internetEnemies.combatCritters.objects.IPackBuilder;
 import com.internetEnemies.combatCritters.objects.ITradeTransactionBuilder;
+import com.internetEnemies.combatCritters.objects.ItemStack;
 import com.internetEnemies.combatCritters.objects.MarketTransaction;
 import com.internetEnemies.combatCritters.objects.MarketTransactionBuilder;
-import com.internetEnemies.combatCritters.objects.Pack;
-import com.internetEnemies.combatCritters.objects.PackBuilder;
 import com.internetEnemies.combatCritters.objects.TradeTransaction;
 import com.internetEnemies.combatCritters.objects.TradeTransactionBuilder;
 
@@ -47,23 +45,126 @@ public class OffersDatabase {
     }
 
     private OffersDatabase() {
-        IRegistry<Pack> packDB = PackCardDatabase.getInstance().getPackDB();
-        IRegistry<Card> cardDB = PackCardDatabase.getInstance().getCardDB();
-        List<TradeTransaction> testTradeOffer = new ArrayList<TradeTransaction>();
-        List<MarketTransaction> testMarketCardOffer = new ArrayList<MarketTransaction>();
-        List<MarketTransaction> testMarketPackOffer = new ArrayList<MarketTransaction>();
-        List<MarketTransaction> testMarketBundleOffer = new ArrayList<MarketTransaction>();
+        List<TradeTransaction> testTradeOffer = new ArrayList<>();
+        List<MarketTransaction> testMarketCardOffer = new ArrayList<>();
+        List<MarketTransaction> testMarketPackOffer = new ArrayList<>();
+        List<MarketTransaction> testMarketBundleOffer = new ArrayList<>();
         IMarketTransactionBuilder marketBuilder = new MarketTransactionBuilder();
         ITradeTransactionBuilder tradesBuilder = new TradeTransactionBuilder();
-        IPackBuilder packBuilder = new PackBuilder();
-        Currency currency = new Currency(100);
-        float discount = 1;
+        Currency testcurrency = new Currency(100);
+        double discount = 1;
 
         // making the instance
-        // 10 Market offers
+        // 10 Market offers (3 pack, 4 card, 3 bundle)
+        // 3 pack offers
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack(PackCardDatabase.getInstance().getPackDB().getSingle(0),1));
+        testMarketBundleOffer.add(marketBuilder.build());
+        marketBuilder.reset();
 
-        // 10 Trade offers
+        testcurrency = new Currency(50);
+        discount = 0.5;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack(PackCardDatabase.getInstance().getPackDB().getSingle(1),1));
+        testMarketBundleOffer.add(marketBuilder.build());
+        marketBuilder.reset();
 
+        testcurrency = new Currency(30);
+        discount = 0;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack(PackCardDatabase.getInstance().getPackDB().getSingle(2),1));
+        testMarketBundleOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        // 4 card offers
+        testcurrency = new Currency(50);
+        discount = 1;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(5),1));
+        testMarketCardOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        testcurrency = new Currency(50);
+        discount = 0.7;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(29),1));
+        testMarketCardOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        testcurrency = new Currency(50);
+        discount = 0.5;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(45),1));
+        testMarketCardOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        testcurrency = new Currency(50);
+        discount = 0.5;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(46),1));
+        testMarketCardOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        // 3 bundle offers
+        testcurrency = new Currency(100);
+        discount = 0.5;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(45),1));
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(46),1));
+        testMarketBundleOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        testcurrency = new Currency(100);
+        discount = 0.8;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(45),1));
+        marketBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getPackDB().getSingle(2),1));
+        testMarketBundleOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        testcurrency = new Currency(50);
+        discount = 0.8;
+        marketBuilder.setPrice(testcurrency);
+        marketBuilder.setDiscount(discount);
+        marketBuilder.addToReceived(new ItemStack(PackCardDatabase.getInstance().getPackDB().getSingle(1),1));
+        marketBuilder.addToReceived(new ItemStack(PackCardDatabase.getInstance().getPackDB().getSingle(2),1));
+        testMarketBundleOffer.add(marketBuilder.build());
+        marketBuilder.reset();
+
+        // 5 Trade offers
+        tradesBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(44),1));
+        tradesBuilder.addToGiven(new ItemStack<IItem>(testcurrency));
+        testTradeOffer.add((TradeTransaction) tradesBuilder.build());
+        tradesBuilder.reset();
+
+        testcurrency = new Currency(200);
+        tradesBuilder.addToReceived(new ItemStack<IItem>(testcurrency));
+        testTradeOffer.add((TradeTransaction) tradesBuilder.build());
+        tradesBuilder.reset();
+
+        tradesBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(30),1));
+        tradesBuilder.addToGiven(new ItemStack<IItem>(PackCardDatabase.getInstance().getPackDB().getSingle(0),1));
+        testTradeOffer.add((TradeTransaction) tradesBuilder.build());
+        tradesBuilder.reset();
+
+        tradesBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(44),1));
+        tradesBuilder.addToGiven(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(40),1));
+        testTradeOffer.add((TradeTransaction) tradesBuilder.build());
+        tradesBuilder.reset();
+
+        tradesBuilder.addToReceived(new ItemStack<IItem>(PackCardDatabase.getInstance().getCardDB().getSingle(44),1));
+        tradesBuilder.addToGiven(new ItemStack<IItem>(PackCardDatabase.getInstance().getPackDB().getSingle(2),1));
+        testTradeOffer.add((TradeTransaction) tradesBuilder.build());
+        tradesBuilder.reset();
 
         tradesDB = new TradeRegistry(testTradeOffer);
         marketDB = new MarketDB(testMarketBundleOffer,testMarketCardOffer,testMarketPackOffer);
