2e24e8e2515c440acbdc26c1fb5f83dd01335fe0
jackal <v@njackal.com>
2024-03-13 17:22:52 -0500

    Untested implementation of PackInventoryHSQLDB

--- app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/PackInventoryHSQLDB.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/PackInventoryHSQLDB.java
@@ -1,13 +1,26 @@
 package com.internetEnemies.combatCritters.data.hsqldb;
 
 import com.internetEnemies.combatCritters.data.IPackInventory;
+import com.internetEnemies.combatCritters.data.hsqldb.DSOHelpers.PackHelper;
 import com.internetEnemies.combatCritters.objects.ItemStack;
 import com.internetEnemies.combatCritters.objects.Pack;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * PackInventoryHSQLDB.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-13
+ *
+ * @PURPOSE:    sql db implementation for pack inventory
+ *              todo -> this hasnt been tested, waiting on the PackHandler in logic to make an integration test
+ */
 public class PackInventoryHSQLDB implements IPackInventory {
 
     private final Connection connection;
@@ -21,12 +34,39 @@ public class PackInventoryHSQLDB implements IPackInventory {
     }
     @Override
     public int getPackAmount(Pack pack) {
-        return 0;
+        int count;
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT amount FROM PackInventory WHERE packId = ?");
+            statement.setInt(1,pack.getId());
+            ResultSet rs = statement.executeQuery();
+
+            if (rs.next()) {
+                count = rs.getInt("amount");
+            } else {
+                count = 0;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error when getting the amount of pack owned");
+        }
+        return count;
     }
 
     @Override
     public void addPack(Pack pack) {
+        int currAmount = getPackAmount(pack);
+        try {
+            PreparedStatement stmt;
+            if (currAmount >0) {
+                stmt = this.connection.prepareStatement("INSERT INTO PackInventory (packId, amount) VALUES (?,1)");
+            } else {
+                stmt = this.connection.prepareStatement("UPDATE PackInventory set amount = amount + 1 WHERE packId = ?");
+            }
+            stmt.setInt(1,pack.getId());
+            stmt.executeUpdate();
 
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while adding pack to inventory",e);
+        }
     }
 
     @Override
@@ -38,16 +78,44 @@ public class PackInventoryHSQLDB implements IPackInventory {
 
     @Override
     public void removePack(Pack pack, int amount) {
+        assert amount > 0;
 
+        int currAmount = getPackAmount(pack);
+        try {
+            PreparedStatement stmt;
+            if(currAmount <= amount) {
+                stmt = this.connection.prepareStatement("DELETE FROM PackInventory WHERE packId = ?");
+                stmt.setInt(1, pack.getId());
+            } else {// curr > amount
+                stmt = this.connection.prepareStatement("UPDATE PackInventory set amount = amount - ? WHERE packId = ?");
+                stmt.setInt(1, amount);
+                stmt.setInt(2, pack.getId());
+            }
+
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while removing pack from inventory",e);
+        }
     }
 
     @Override
     public void removePack(Pack pack) {
-
+        removePack(pack, 1);
     }
 
     @Override
     public List<ItemStack<Pack>> getPacks() {
-        return null;
+        List<ItemStack<Pack>> packs = new ArrayList<>();
+        try {
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM Packs LEFT JOIN PackInventory ON Packs.id = PackInventory.packId");
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()) {
+                Pack pack = PackHelper.packFromResultSet(rs, connection);
+                packs.add(new ItemStack<>(pack,rs.getInt("amount")));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("failed getting list of packs from inventory",e);
+        }
+        return packs;
     }
 }
