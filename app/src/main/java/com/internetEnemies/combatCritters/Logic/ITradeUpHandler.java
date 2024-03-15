580dbc7b7c33881a7e4597244349b68f53f7d8e9
Hugo Ng <ym7ym7228@gmail.com>
2024-03-15 16:11:18 -0500

    Created: ITradeUpHandler & ITradeUpValidator

---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/ITradeUpHandler.java
@@ -0,0 +1,13 @@
+/**
+ * ITradeUpHandler.java
+ * COMP 3350 A02
+ * @Project      combat critters
+ * @created      15-March-2024
+ *
+ * @PURPOSE:     Interface of handling Trade Up, add/ remove card from Crd Inventoryy
+ */
+
+package com.internetEnemies.combatCritters.Logic;
+
+public interface ITradeUpHandler {
+}
---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/ITradeUpValidator.java
@@ -0,0 +1,12 @@
+/**
+ * ITradeUpValidator.java
+ * COMP 3350 A02
+ * @Project      combat critters
+ * @created      15-March-2024
+ *
+ * @PURPOSE:     Interface of validate trade up transaction
+ */
+package com.internetEnemies.combatCritters.Logic;
+
+public interface ITradeUpValidator {
+}
