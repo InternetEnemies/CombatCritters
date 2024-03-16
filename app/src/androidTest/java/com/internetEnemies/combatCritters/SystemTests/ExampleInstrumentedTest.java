b7d31f3d332a19ef1f03d1b97a6cbed82a593d11
LucasBerzuk <lucasberzuk@icloud.com>
2024-03-16 13:16:05 -0500

    yer

--- app/src/androidTest/java/com/internetEnemies/combatCritters/SystemTests/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/internetEnemies/combatCritters/SystemTests/ExampleInstrumentedTest.java
@@ -1,27 +1,47 @@
 package com.internetEnemies.combatCritters.SystemTests;
 
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 import android.content.Context;
 
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.filters.LargeTest;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.Rule;
 
 import static org.junit.Assert.*;
+
+import com.internetEnemies.combatCritters.R;
+import com.internetEnemies.combatCritters.presentation.MainMenuActivity;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  */
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityScenarioRule<MainMenuActivity> mActivityRule = new ActivityScenarioRule<>(MainMenuActivity.class);
+
     @Test
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("internetEnemies.combatCritters", appContext.getPackageName());
+        assertEquals("com.internetEnemies.combatCritters", appContext.getPackageName());
+    }
+
+    @Test
+    public void testButtonClick() {
+        onView(withId(R.id.buttonToDeckBuilder)).perform(click());
     }
 }
 
