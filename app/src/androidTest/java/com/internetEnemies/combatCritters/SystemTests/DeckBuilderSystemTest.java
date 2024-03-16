edb97cf2747e9e777a278240fcd50ca9c83be7e5
LucasBerzuk <lucasberzuk@icloud.com>
2024-03-16 16:32:11 -0500

    pain, cleaned up a bit

--- app/src/androidTest/java/com/internetEnemies/combatCritters/SystemTests/DeckBuilderSystemTest.java
+++ app/src/androidTest/java/com/internetEnemies/combatCritters/SystemTests/DeckBuilderSystemTest.java
@@ -3,50 +3,24 @@ package com.internetEnemies.combatCritters.SystemTests;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import androidx.test.espresso.action.ViewActions;
-import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
-import static androidx.test.espresso.contrib.RecyclerViewActions.*;
 
-
-import android.content.Context;
-import android.view.View;
-import android.widget.GridView;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.filters.LargeTest;
-import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import org.hamcrest.Matcher;
-import org.hamcrest.core.AllOf;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.Rule;
 
-import static org.junit.Assert.*;
-
-import static java.util.EnumSet.allOf;
-
 import com.internetEnemies.combatCritters.R;
-import com.internetEnemies.combatCritters.presentation.BuilderFragment;
-import com.internetEnemies.combatCritters.presentation.InventoryFragment;
-import com.internetEnemies.combatCritters.presentation.InventoryViewModel;
-import com.internetEnemies.combatCritters.presentation.ItemGridFragment;
 import com.internetEnemies.combatCritters.presentation.MainMenuActivity;
 
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -62,25 +36,8 @@ public class DeckBuilderSystemTest {
 
     @Test
     public void testDeckBuilder() {
-        ItemGridFragment itemGridFragment;
-        
         // Click deck builder button
         onView(withId(R.id.buttonToDeckBuilder)).perform(click());
-
-        mActivityRule.getScenario().onActivity(activity -> {
-            FragmentManager fragmentManager = activity.getSupportFragmentManager();
-            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-            InventoryFragment inventoryFragment = new InventoryFragment();
-            BuilderFragment builderFragment = new BuilderFragment();
-
-            // Add both fragments to the activity
-            fragmentTransaction.add(inventoryFragment, null);
-            fragmentTransaction.add(builderFragment, null);
-            //fragmentTransaction.add(itemGridFragment, null);
-
-            // Commit the transaction
-            fragmentTransaction.commit();
-        });
 
         // Flip the showall slider
         onView(withId(R.id.showall_toggle)).perform(click());
@@ -106,7 +63,7 @@ public class DeckBuilderSystemTest {
                 .perform(ViewActions.click());
 
         // Select waffle warrior
-        onView(withId(itemGridFragment.getView().findViewById(R.id.cardGridView))).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        onView(withId(R.id.cardGridView)).perform(click());
 
         // Click add to deck 20 times
 
