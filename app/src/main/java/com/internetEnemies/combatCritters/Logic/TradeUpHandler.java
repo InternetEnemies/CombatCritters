6394f563498aceb5553365121210799bd904b65d
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-19 02:39:26 -0500

    created: TradeUpHandler

---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/TradeUpHandler.java
@@ -0,0 +1,37 @@
+/**
+ * TradeUpHandler.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-19
+ *
+ * @PURPOSE:    implementation of ITradeUpValidator
+ */
+
+package com.internetEnemies.combatCritters.Logic;
+
+import com.internetEnemies.combatCritters.objects.Card;
+import com.internetEnemies.combatCritters.objects.ItemStack;
+
+import java.util.List;
+
+public class TradeUpHandler implements ITradeUpHandler{
+    @Override
+    public List<ItemStack<Card>> getCards(Card.Rarity rarity) {
+        return null;
+    }
+
+    @Override
+    public void addCard(Card card) {
+
+    }
+
+    @Override
+    public void removeCard(Card card) {
+
+    }
+
+    @Override
+    public boolean confirmTradeUp() {
+        return false;
+    }
+}
