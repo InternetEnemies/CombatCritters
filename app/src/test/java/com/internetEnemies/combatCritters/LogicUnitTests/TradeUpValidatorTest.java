e5b768e5d12f53a55c0493dab067a5687d4bc159
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-19 02:34:49 -0500

    updated: TradeUpValidatorTest: tests

--- app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/TradeUpValidatorTest.java
+++ app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/TradeUpValidatorTest.java
@@ -13,10 +13,31 @@ import static org.mockito.Mockito.*;
 import com.internetEnemies.combatCritters.Logic.ITradeUpValidator;
 
 import org.junit.Before;
+import org.junit.Test;
 
 public class TradeUpValidatorTest {
     @Before
     public void setup(){
         ITradeUpValidator ITradeUpValidatorMock = mock(ITradeUpValidator.class);
+    }
+
+    @Test
+    public void testNormalCase(){
+
+    }
+
+    @Test
+    public void testDifferentRarity(){
+
+    }
+
+    @Test
+    public void testCardOverwhelmed(){
+
+    }
+
+    @Test
+    public void testNotEnoughCards(){
+
     }
 }
