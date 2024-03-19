1ea781b94c9655bb8b12c8b2cf6a3a2fa3f6017e
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-19 02:41:26 -0500

    created: TradeUpValidator

---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/TradeUpValidator.java
@@ -0,0 +1,19 @@
+/**
+ * TradeUpValidator.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-19
+ *
+ * @PURPOSE:    implementation of ITradeUpValidator
+ */
+
+package com.internetEnemies.combatCritters.Logic;
+
+import com.internetEnemies.combatCritters.objects.TradeTransaction;
+
+public class TradeUpValidator implements ITradeUpValidator{
+    @Override
+    public boolean validate(TradeTransaction tradeUp) {
+        return false;
+    }
+}
