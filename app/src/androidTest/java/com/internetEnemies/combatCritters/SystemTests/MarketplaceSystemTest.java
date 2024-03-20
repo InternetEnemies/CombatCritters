6fae8a39195585e9c8d47ec6ed1439c2836bbe56
LucasBerzuk <lucasberzuk@icloud.com>
2024-03-20 13:50:50 -0500

    MarketplaceSystemTest good, added currency at the start then check if currency after purchase != starting currency

--- app/src/androidTest/java/com/internetEnemies/combatCritters/SystemTests/MarketplaceSystemTest.java
+++ app/src/androidTest/java/com/internetEnemies/combatCritters/SystemTests/MarketplaceSystemTest.java
@@ -2,13 +2,8 @@ package com.internetEnemies.combatCritters.SystemTests;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
-import static org.hamcrest.CoreMatchers.allOf;
-
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.matcher.RootMatchers;
 import androidx.test.espresso.matcher.ViewMatchers;
@@ -18,16 +13,13 @@ import androidx.test.filters.LargeTest;
 
 import com.internetEnemies.combatCritters.R;
 import com.internetEnemies.combatCritters.SystemTests.Assertions.RecyclerCountAssertion;
-import com.internetEnemies.combatCritters.objects.MarketTransaction;
-import com.internetEnemies.combatCritters.presentation.ItemAdapter;
+import com.internetEnemies.combatCritters.data.Database;
+import com.internetEnemies.combatCritters.objects.Currency;
 import com.internetEnemies.combatCritters.presentation.MainMenuActivity;
-import com.internetEnemies.combatCritters.presentation.MarketplaceActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.List;
 
 /**
  * MarketplaceSystemTest.java
@@ -45,15 +37,24 @@ public class MarketplaceSystemTest {
     public ActivityScenarioRule<MainMenuActivity> mActivityRule = new ActivityScenarioRule<>(MainMenuActivity.class);
 
     @Test
-    public void testCurrency() {
+    public void testMarketplace() {
+        // Add currency to the test because we have 0 on start
+        int value = 100;
+        Currency startValue = new Currency(value);
+        Database.getInstance().getCurrencyInventory().addToBalance(startValue);
+
         // Click marketplace button
         onView(withId(R.id.buttonToMarketplace)).perform(click());
 
         // Click the card you want to purchase
-        onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(1, click()));
+        onView(withId(R.id.recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
         // Click the purchase button
         onView(ViewMatchers.withText("Purchase")).inRoot(RootMatchers.isDialog()).perform(click());
+
+        // Check if the currency is not the same as when we started (i.e. we made a purchase)
+        Currency endValue = Database.getInstance().getCurrencyInventory().getCurrentBalance();
+        assert(!startValue.equals(endValue));
 
         // Click the main menu button
         onView(withId(R.id.mainMenuButton)).perform(click());
