6197c478cb24faa2e6c4842220fd81690ad8093f
chtadena <149117831+chtadena@users.noreply.github.com>
2024-03-21 02:45:57 -0500

    finished implementation of MarketCycle.java

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/MarketCycle.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/MarketCycle.java
@@ -4,35 +4,56 @@ import com.internetEnemies.combatCritters.data.IMarketDB;
 import com.internetEnemies.combatCritters.data.MarketDB;
 import com.internetEnemies.combatCritters.objects.MarketTransaction;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-//TODO: Maybe switch to datetime?
+/**
+ * MarketCycle.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-21
+ *
+ * @PURPOSE:    Implementation of IMarketCycle
+ */
 public class MarketCycle implements IMarketCycle{
 
     public final int NUM_DISCOUNTS = 3;
+    public final double MAX_DISCOUNT_VAL = 0.85;
+    public final double MIN_DISCOUNT_VAL = 0.1;
 
-    private final LocalTime refreshTime;
+    private final int hours;
+
+    private final int mins;
+    private LocalDateTime refreshTime;
     private final IMarketDB marketDB;
+    private LocalDateTime currentTime;
 
     public MarketCycle(IMarketDB marketDB){
-        refreshTime = LocalTime.of(LocalTime.MIDNIGHT.getHour(), LocalTime.MIDNIGHT.getMinute()); //should refresh at midnight GMT
+        hours = 23;
+        mins = 59;
+        refreshTime = LocalDateTime.now().with(LocalTime.of(hours, mins));
         this.marketDB = marketDB;
     }
 
     public MarketCycle(int hours, int mins, IMarketDB marketDB){
-        refreshTime = LocalTime.of(hours, mins);
+        this.mins = mins;
+        this.hours = hours;
+        refreshTime = LocalDateTime.now().with(LocalTime.of(this.hours, this.mins));
         this.marketDB = marketDB;
     }
     @Override
     public void applyDiscounts(Map<Integer, Double> discounts) {
-        LocalTime currentTime = LocalTime.now();
-
         if(currentTime.isAfter(refreshTime)){
             marketDB.adjustDiscount(discounts);
+            refreshTime = LocalDateTime.now().with(LocalTime.of(this.hours, this.mins));
         }
+    }
+
+    public void setCurrentTime(){
+        currentTime = LocalDateTime.now();
     }
 
     public void applyDiscounts() {
@@ -53,14 +74,12 @@ public class MarketCycle implements IMarketCycle{
         {
             // generating the index using Math.random()
             int index = (int)(Math.random() * allTransactions.size());
-
-
+            double discount = MIN_DISCOUNT_VAL + Math.random() * (MAX_DISCOUNT_VAL - MIN_DISCOUNT_VAL);
+            adjustedDiscounts.put(index,discount);
+            allTransactions.remove(index);
         }
 
-
-
-
-
+        return adjustedDiscounts;
     }
 
 }
