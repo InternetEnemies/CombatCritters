835efadcaf1fcb6b64aaf7ef9d0640cfb0b0a0dc
jackal <v@njackal.com>
2024-03-23 13:49:24 -0500

    battle docs

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/battles/BattleOrchestrator.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/battles/BattleOrchestrator.java
@@ -2,6 +2,14 @@ package com.internetEnemies.combatCritters.Logic.battles;
 
 import com.internetEnemies.combatCritters.objects.Card;
 
+/**
+ * BattleOrchestrator.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-23
+ *
+ * @PURPOSE:    Orchestrate the communication of battle state to the ui and player input to a battle
+ */
 public class BattleOrchestrator implements IBattleOrchestrator {
     private IBattleStateObserver observer;
     public BattleOrchestrator(IBattleStateObserver observer, Battle battle) {
--- app/src/main/java/com/internetEnemies/combatCritters/Logic/battles/IBattleFactory.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/battles/IBattleFactory.java
@@ -4,6 +4,14 @@ import com.internetEnemies.combatCritters.objects.Card;
 
 import java.util.List;
 
+/**
+ * IBattleFactory.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-23
+ *
+ * @PURPOSE:    interface for battle factories which provide BattleOrchestrators to use
+ */
 public interface IBattleFactory {
     /**
      * Factory method for Battles / BattleOrchestrators
