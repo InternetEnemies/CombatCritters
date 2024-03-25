cbb80f4aba692afbd1e5287fdb8b81225c39144d
Kevin Breckman <kevinbreckman@gmail.com>
2024-03-24 21:07:29 -0500

    get cards to add to recyclers

--- app/src/main/java/com/internetEnemies/combatCritters/presentation/TradeUpActivity.java
+++ app/src/main/java/com/internetEnemies/combatCritters/presentation/TradeUpActivity.java
@@ -7,12 +7,14 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.internetEnemies.combatCritters.Logic.ITradeUpHandler;
 import com.internetEnemies.combatCritters.Logic.TradeUpHandler;
 import com.internetEnemies.combatCritters.databinding.ActivityTradeUpBinding;
 import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.ItemStack;
+import com.internetEnemies.combatCritters.presentation.renderable.CardRenderer;
 import com.internetEnemies.combatCritters.presentation.renderable.CardStackRenderer;
 
 import java.util.ArrayList;
@@ -26,26 +28,29 @@ import java.util.ArrayList;
  * @PURPOSE:
  */
 public class TradeUpActivity extends AppCompatActivity {
-    private ActivityTradeUpBinding binding;
-    private ItemAdapter<ItemStack<Card>> cardStackAdapter;
+    private ItemAdapter<ItemStack<Card>> inventoryAdapter;
+    private ItemAdapter<Card> tradeUpAdapter;
     private ITradeUpHandler tradeUpHandler;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityTradeUpBinding.inflate(getLayoutInflater());
+        com.internetEnemies.combatCritters.databinding.ActivityTradeUpBinding binding = ActivityTradeUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        this.tradeUpHandler = LogicProvider.getInstance().getTradeUpHandler();
-        this.cardStackAdapter = new ItemAdapter<>(CardStackRenderer.getRenderers(tradeUpHandler.getCards(), this), this::cardClicked, false);
+        this.tradeUpHandler = new TradeUpHandler();//LogicProvider.getInstance().getTradeUpHandler();
+        this.inventoryAdapter = new ItemAdapter<>(CardStackRenderer.getRenderers(tradeUpHandler.getCards(), this), this::inventoryCardClicked, false);
+        this.tradeUpAdapter = new ItemAdapter<>(new ArrayList<>(), this::tradeUpCardClicked, false);
 
-        Log.d("here", String.valueOf(tradeUpHandler.getCards().size()));
         binding.inventoryRecyclerView.setLayoutManager(new GridLayoutManager(this, 6));
         binding.inventoryRecyclerView.addItemDecoration(new SpacingItemDecoration());
-        binding.inventoryRecyclerView.setAdapter(cardStackAdapter);
+        binding.inventoryRecyclerView.setAdapter(inventoryAdapter);
 
-
+        binding.tradeUpRecyclerView.setLayoutManager(new GridLayoutManager(this, 5));
+        binding.tradeUpRecyclerView.addItemDecoration(new SpacingItemDecoration());
+        binding.tradeUpRecyclerView.setAdapter(tradeUpAdapter);
 
         binding.mainMenuButton.setOnClickListener((View buttonView) -> {
             Intent intent = new Intent(TradeUpActivity.this, MainMenuActivity.class);
@@ -53,7 +58,16 @@ public class TradeUpActivity extends AppCompatActivity {
         });
     }
 
-    private void cardClicked(ItemStack<Card> card) {
+    private void inventoryCardClicked(ItemStack<Card> cardStack) {
+        tradeUpHandler.addCard(cardStack.getItem());
+        tradeUpAdapter.updateItems(CardRenderer.getRenderers(tradeUpHandler.getSelectedCards(), this));
+        inventoryAdapter.updateItems(CardStackRenderer.getRenderers(tradeUpHandler.getCards(), this));
+//        Log.d("here", String.valueOf(tradeUpHandler.getCards().get(0).getAmount()));
+    }
 
+    private void tradeUpCardClicked(Card card) {
+        tradeUpHandler.removeCard(card);
+        tradeUpAdapter.updateItems(CardRenderer.getRenderers(tradeUpHandler.getSelectedCards(), this));
+        inventoryAdapter.updateItems(CardStackRenderer.getRenderers(tradeUpHandler.getCards(), this));
     }
 }
