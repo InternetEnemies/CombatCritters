05c367318b78875de0d1f2c24879f16071fd576c
Hugo Ng <ym7ym7228@gmail.com>
2024-03-25 16:42:41 -0500

    created: custom exception

---
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/exceptions/InvalidTradeUpCardsException.java
@@ -0,0 +1,16 @@
+/**
+ * InvalidTradeUpCardsException.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-25
+ *
+ * @PURPOSE:    custom exception to throws in TradeUpHandler.confirmTradeUp()
+ */
+
+package com.internetEnemies.combatCritters.Logic.exceptions;
+
+public class InvalidTradeUpCardsException extends RuntimeException{
+    public InvalidTradeUpCardsException(int difference){
+        super("Receive invalid Trade Up cards:\n Difference: "+difference);
+    }
+}
