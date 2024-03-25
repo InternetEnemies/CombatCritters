41890c0603f4cf6108c65c973c82a8ddee12ad94
Kevin Breckman <kevinbreckman@gmail.com>
2024-03-25 15:05:15 -0500

    Add card popup when tradeup is clicked

--- app/src/main/java/com/internetEnemies/combatCritters/presentation/TradeUpActivity.java
+++ app/src/main/java/com/internetEnemies/combatCritters/presentation/TradeUpActivity.java
@@ -1,10 +1,13 @@
 package com.internetEnemies.combatCritters.presentation;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -14,11 +17,14 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.internetEnemies.combatCritters.Logic.ITradeUpHandler;
 import com.internetEnemies.combatCritters.Logic.TradeUpHandler;
 import com.internetEnemies.combatCritters.R;
+import com.internetEnemies.combatCritters.data.Database;
 import com.internetEnemies.combatCritters.databinding.ActivityTradeUpBinding;
 import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.ItemStack;
+import com.internetEnemies.combatCritters.objects.MarketTransaction;
 import com.internetEnemies.combatCritters.presentation.renderable.CardRenderer;
 import com.internetEnemies.combatCritters.presentation.renderable.CardStackRenderer;
+import com.internetEnemies.combatCritters.presentation.renderable.MysteryCardRenderer;
 
 import java.util.ArrayList;
 
@@ -31,21 +37,27 @@ import java.util.ArrayList;
  * @PURPOSE:
  */
 public class TradeUpActivity extends AppCompatActivity {
+    ActivityTradeUpBinding binding;
     private ItemAdapter<ItemStack<Card>> inventoryAdapter;
     private ItemAdapter<Card> tradeUpAdapter;
     private ITradeUpHandler tradeUpHandler;
     private ViewGroup mysteryCardContainer;
+    private ImageView rightArrow;
+    private Button tradeUpButton;
     private static final float SCALE_FACTOR = .65f; //Scale factor for trade up cards
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        com.internetEnemies.combatCritters.databinding.ActivityTradeUpBinding binding = ActivityTradeUpBinding.inflate(getLayoutInflater());
+        this.binding = ActivityTradeUpBinding.inflate(getLayoutInflater());
+//        com.internetEnemies.combatCritters.databinding.ActivityTradeUpBinding binding = ActivityTradeUpBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        this.tradeUpButton = findViewById(R.id.tradeUpButton);
+        this.rightArrow = findViewById(R.id.rightArrow);
         this.mysteryCardContainer = findViewById(R.id.mysteryCardContainer);
-        this.tradeUpHandler = new TradeUpHandler();//LogicProvider.getInstance().getTradeUpHandler();
+        this.tradeUpHandler = LogicProvider.getInstance().getTradeUpHandler();
         this.inventoryAdapter = new ItemAdapter<>(CardStackRenderer.getRenderers(tradeUpHandler.getCards(), this), this::inventoryCardClicked, false);
         this.tradeUpAdapter = new ItemAdapter<>(new ArrayList<>(), this::tradeUpCardClicked, false);
 
@@ -54,8 +66,10 @@ public class TradeUpActivity extends AppCompatActivity {
         binding.inventoryRecyclerView.setAdapter(inventoryAdapter);
 
         binding.tradeUpRecyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
-        binding.tradeUpRecyclerView.addItemDecoration(new SpacingItemDecoration(-10, 10));
+        binding.tradeUpRecyclerView.addItemDecoration(new SpacingItemDecoration(0, 0));
         binding.tradeUpRecyclerView.setAdapter(tradeUpAdapter);
+
+        tradeUpButton.setOnClickListener(v -> tradeUpButtonClicked());
 
         binding.mainMenuButton.setOnClickListener((View buttonView) -> {
             Intent intent = new Intent(TradeUpActivity.this, MainMenuActivity.class);
@@ -63,16 +77,80 @@ public class TradeUpActivity extends AppCompatActivity {
         });
     }
 
+
+    private void tradeUpButtonClicked() {
+        try {
+//            tradeUpHandler.confirmTradeUp();
+            Log.d("here", getCardView().toString());
+            showCardPopupFragment(getCard());
+        }
+        catch(Exception e) {
+
+        }
+    }
+
     private void inventoryCardClicked(ItemStack<Card> cardStack) {
         tradeUpHandler.addCard(cardStack.getItem());
         tradeUpAdapter.updateItems(CardRenderer.getRenderers(tradeUpHandler.getSelectedCards(), this, SCALE_FACTOR));
         inventoryAdapter.updateItems(CardStackRenderer.getRenderers(tradeUpHandler.getCards(), this));
-//        mysteryCardContainer.addView((new MysterCardRenderer(tradeUpHandler.get)))
+
+        showTradeUpPreview(isValid());
     }
 
     private void tradeUpCardClicked(Card card) {
         tradeUpHandler.removeCard(card);
         tradeUpAdapter.updateItems(CardRenderer.getRenderers(tradeUpHandler.getSelectedCards(), this, SCALE_FACTOR));
         inventoryAdapter.updateItems(CardStackRenderer.getRenderers(tradeUpHandler.getCards(), this));
+
+        showTradeUpPreview(isValid());
+    }
+
+    private void showTradeUpPreview(boolean show) {
+        if(show) {
+            rightArrow.setVisibility(View.VISIBLE);
+            binding.mysteryCardContainer.removeAllViews();
+            mysteryCardContainer.addView((new MysteryCardRenderer(tradeUpHandler.getCurrentTradeUpRarity(), this)).getView(null, mysteryCardContainer));
+            tradeUpButton.setVisibility(View.VISIBLE);
+        }
+        else {
+            rightArrow.setVisibility(View.INVISIBLE);
+            mysteryCardContainer.removeAllViews();
+            tradeUpButton.setVisibility(View.INVISIBLE);
+        }
+        tradeUpButton.setVisibility(View.VISIBLE); //TODO remove this
+    }
+
+    private void showCardPopupFragment(Card card) {
+        CardPopupFragment fragment = CardPopupFragment.newInstance(card);
+        fragment.show(getSupportFragmentManager(), "card_popup");
+    }
+
+    //TODO get rid of this and the other
+    private boolean isValid() {
+        if(tradeUpHandler.getSelectedCards().size() == 5 ) {
+            return true;
+        }
+        return false;
+    }
+
+    private Card getCard() {
+        return  Database.getInstance().getCardInventory().getCards().get(0).getItem();
+    }
+
+    private View getCardView() {
+        Card card = Database.getInstance().getCardInventory().getCards().get(0).getItem();
+        return (new CardRenderer(card, this).getView(null, null));
+    }
+
+    private void showCardPopup() {
+        AlertDialog dialog = new AlertDialog.Builder(this)
+                .setTitle("Custom Dialog")
+                .setView(getCardView())
+                .setPositiveButton("Close", (dialogInterface, i) -> {
+                    dialogInterface.dismiss();
+                })
+                .create();
+
+        dialog.show();
     }
 }
