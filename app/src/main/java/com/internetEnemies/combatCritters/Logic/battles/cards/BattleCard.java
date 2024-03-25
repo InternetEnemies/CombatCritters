1b4a701c57f65e810391299a789a67c88f07cd64
jackal <v@njackal.com>
2024-03-25 09:31:15 -0500

    BattleCard now has the capacity to store its parent and opposing row

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/battles/cards/BattleCard.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/battles/cards/BattleCard.java
@@ -1,6 +1,7 @@
 package com.internetEnemies.combatCritters.Logic.battles.cards;
 
 import com.internetEnemies.combatCritters.Logic.battles.stateHandlers.Health;
+import com.internetEnemies.combatCritters.Logic.battles.stateHandlers.IBoard;
 import com.internetEnemies.combatCritters.Logic.battles.stateHandlers.IHealth;
 import com.internetEnemies.combatCritters.objects.CritterCard;
 import com.internetEnemies.combatCritters.objects.battles.CardState;
@@ -8,10 +9,14 @@ import com.internetEnemies.combatCritters.objects.battles.CardState;
 public class BattleCard {
     private final CritterCard card;
     private final IHealth health;
+    private IBoard parent;
+    private IBoard opposing;
+    int slot; // ideally state like this would only be held in one place //todo maybe store the slot in a map in the parent
 
     public BattleCard(CritterCard card, IHealth health) {
         this.card = card;
         this.health = health;
+        this.slot = -1;
     }
     public BattleCard(CritterCard card) {
         this(
@@ -26,5 +31,17 @@ public class BattleCard {
 
     public IHealth getHealth() {
         return this.health;
+    }
+
+    /**
+     * play the card at a position in a row with an opposing row
+     * @param slot slot card is played at
+     * @param parent row this card belongs to
+     * @param opposing row this card is against (can be null)
+     */
+    public void playAt(int slot, IBoard parent, IBoard opposing){
+        this.slot = slot;
+        this.parent = parent;
+        this.opposing = opposing;
     }
 }
