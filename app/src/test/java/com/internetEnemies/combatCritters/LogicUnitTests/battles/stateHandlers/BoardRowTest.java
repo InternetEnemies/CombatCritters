b9bf7391af42a464a10d6cf1ae2e9aa193370761
jackal <v@njackal.com>
2024-03-26 15:30:12 -0500

    BoardRowTests

---
+++ app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/battles/stateHandlers/BoardRowTest.java
@@ -0,0 +1,91 @@
+package com.internetEnemies.combatCritters.LogicUnitTests.battles.stateHandlers;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import com.internetEnemies.combatCritters.Logic.battles.cards.BattleCard;
+import com.internetEnemies.combatCritters.Logic.battles.cards.IBattleCard;
+import com.internetEnemies.combatCritters.Logic.battles.events.EventSystem;
+import com.internetEnemies.combatCritters.Logic.battles.events.IEventSystem;
+import com.internetEnemies.combatCritters.Logic.battles.exceptions.BattleException;
+import com.internetEnemies.combatCritters.Logic.battles.stateHandlers.BoardRow;
+import com.internetEnemies.combatCritters.Logic.battles.stateHandlers.IBoardRow;
+import com.internetEnemies.combatCritters.LogicUnitTests.battles.EventFlag;
+
+public class BoardRowTest {
+    private static final int ROW_SIZE = 5;
+    IBoardRow row;
+    IEventSystem eventSystem;
+    IBattleCard sampleCard;
+    @Before
+    public void setup() {
+        sampleCard = mock(BattleCard.class);
+        eventSystem = new EventSystem();
+        row = new BoardRow(
+                eventSystem,
+                ROW_SIZE,
+                new BattleCard[]{
+                        null,
+                        null,
+                        null,
+                        null,
+                        null
+        });
+    }
+
+    @Test
+    public void testPlayGetCard() throws BattleException {
+        assertNull(row.getCard(0));
+        row.playCard(0, sampleCard);
+        assertNotNull(row.getCard(0));
+    }
+
+    @Test(expected = BattleException.class)
+    public void testFailPlayCard() throws BattleException{
+        row.playCard(0,sampleCard);
+        row.playCard(0,sampleCard);
+    }
+
+    @Test
+    public void testPlayCardEvent() throws BattleException {
+        EventFlag flag = new EventFlag();
+        eventSystem.getOnPlayCard().subscribe(event -> {
+            assertEquals(sampleCard, event.getCard());
+            assertEquals(1, event.getPos());
+            assertEquals(row, event.getRow());
+            flag.fire();
+        });
+        row.playCard(1,sampleCard);
+        assertTrue(flag.hasFired());
+    }
+
+    @Test
+    public void testKillCard() throws BattleException {
+        row.playCard(0,sampleCard);
+        assertNotNull(row.getCard(0));
+        row.killCard(0);
+        assertNull(row.getCard(0));
+    }
+
+    @Test(expected = BattleException.class)
+    public void testKillCardFailure() throws BattleException {
+        row.killCard(0);
+    }
+
+    @Test
+    public void testKillCardEvent() throws BattleException{
+        EventFlag flag = new EventFlag();
+        eventSystem.getOnCardKilled().subscribe(event -> {
+            assertEquals(sampleCard, event.getCard());
+            assertEquals(1, event.getPos());
+            assertEquals(row, event.getRow());
+            flag.fire();
+        });
+        row.playCard(1,sampleCard);
+        row.killCard(1);
+        assertTrue(flag.hasFired());
+    }
+}
