d6dd300331e8294685314500e2ee13f58c1a9f89
Hugo Ng <ym7ym7228@gmail.com>
2024-03-27 12:46:39 -0500

    created: Opponent

---
+++ app/src/main/java/com/internetEnemies/combatCritters/objects/battles/Opponent.java
@@ -0,0 +1,12 @@
+package com.internetEnemies.combatCritters.objects.battles;
+
+/**
+ * Opponent.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     3/27/24
+ *
+ * @PURPOSE: Opponents DSO for the battle
+ */
+public class Opponent {
+}
