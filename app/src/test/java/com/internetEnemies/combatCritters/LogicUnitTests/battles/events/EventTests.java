9627dd0f5063643b6613d2560f7ed970b2f1a107
jackal <v@njackal.com>
2024-03-27 14:11:10 -0500

    updated EventTests

--- app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/battles/events/EventTests.java
+++ app/src/test/java/com/internetEnemies/combatCritters/LogicUnitTests/battles/events/EventTests.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.*;
 import com.internetEnemies.combatCritters.Logic.battles.cards.IBattleCard;
 import com.internetEnemies.combatCritters.Logic.battles.events.BoardEvent;
 import com.internetEnemies.combatCritters.Logic.battles.events.CardEvent;
+import com.internetEnemies.combatCritters.Logic.battles.events.CardHealthEvent;
 import com.internetEnemies.combatCritters.Logic.battles.stateHandlers.IBoardRow;
 
 /**
@@ -34,5 +35,13 @@ public class EventTests {
         IBattleCard card = mock(IBattleCard.class);
         CardEvent event = new CardEvent(1, row, card);
         assertEquals(card, event.getCard());
+    }
+    @Test
+    public void test_CardHealthEvent() {
+        IBoardRow row = mock(IBoardRow.class);
+        IBattleCard card = mock(IBattleCard.class);
+        CardHealthEvent event = new CardHealthEvent(1, row, card, 2);
+        assertEquals(2, event.getHealthChange());
+
     }
 }
