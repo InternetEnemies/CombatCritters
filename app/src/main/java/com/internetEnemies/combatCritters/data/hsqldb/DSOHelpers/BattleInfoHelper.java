8487b98d68dadb839f01402a2d037a420fe322c5
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-27 17:22:51 -0500

    updated: BattleInfoHelper: opponentFromResultSet

--- app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/DSOHelpers/BattleInfoHelper.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/DSOHelpers/BattleInfoHelper.java
@@ -1,6 +1,10 @@
 package com.internetEnemies.combatCritters.data.hsqldb.DSOHelpers;
 
+import com.internetEnemies.combatCritters.data.hsqldb.RewardTransactionBuilder;
+import com.internetEnemies.combatCritters.data.OffersDatabase;
+import com.internetEnemies.combatCritters.objects.battles.IRewardTransactionBuilder;
 import com.internetEnemies.combatCritters.objects.battles.Opponent;
+import com.internetEnemies.combatCritters.objects.battles.RewardTransaction;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -15,7 +19,22 @@ import java.sql.SQLException;
  */
 
 public class BattleInfoHelper {
+    /**
+     * get a opponent object form the result set
+     * @param rs result set to get
+     * @return opponent object from the ResultSet
+     */
     public static Opponent opponentFromResultSet(final ResultSet rs) throws SQLException{
+        final  int id = rs.getInt("id");
+        final String name = rs.getString("name");
+        final String description = rs.getString("DESCRIPTION");
+        final String imageRef = rs.getString("IMAGE");
+        final int rewardsID = rs.getInt("REWARDSID");
 
+
+        IRewardTransactionBuilder builder = new RewardTransactionBuilder();
+        builder.fromTransaction(OffersDatabase.getInstance().getRewardDB().getSingle(rewardsID));
+        RewardTransaction reward = builder.build();
+        return new Opponent(id,name,description,imageRef,reward);
     }
 }
