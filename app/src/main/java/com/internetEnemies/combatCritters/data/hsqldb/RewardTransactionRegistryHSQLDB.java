c5c9e3db1aa9788ec00c242f66a0ab455c363e2d
Hugo Ng <ym7ym7228@gmail.com>
2024-03-27 13:41:02 -0500

    created: RewardTransactionRegistryHSQLDB

---
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/RewardTransactionRegistryHSQLDB.java
@@ -0,0 +1,13 @@
+package com.internetEnemies.combatCritters.data.hsqldb;
+
+/**
+ * RewardTransactionRegistryHSQLDB.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     2024-03-27
+ *
+ * @PURPOSE     IRegistry implementation  for sql db of RewardTransaction
+ */
+
+public class RewardTransactionRegistryHSQLDB {
+}
