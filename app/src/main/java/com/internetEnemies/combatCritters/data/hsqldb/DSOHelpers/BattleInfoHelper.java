01101b5cd28f26f0e0df75d76d5a0d4f2ca43a62
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-27 17:03:11 -0500

    created: BattleInfoHelper

---
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/DSOHelpers/BattleInfoHelper.java
@@ -0,0 +1,21 @@
+package com.internetEnemies.combatCritters.data.hsqldb.DSOHelpers;
+
+import com.internetEnemies.combatCritters.objects.battles.Opponent;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * BattleInfoHelper.java
+ * COMP 3350 A02
+ * @Project     Combat Critters
+ * @created     27/7/24
+ *
+ * @PURPOSE:    help the database to make opponent objects
+ */
+
+public class BattleInfoHelper {
+    public static Opponent opponentFromResultSet(final ResultSet rs) throws SQLException{
+
+    }
+}
