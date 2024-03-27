5b29443dee1b2b95c3729e200452aba43f8a3dc4
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-27 17:34:51 -0500

    updated: BattleInfoRegistryHSQLDB: implements default methods

--- app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/BattleInfoRegistryHSQLDB.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/BattleInfoRegistryHSQLDB.java
@@ -1,8 +1,16 @@
 package com.internetEnemies.combatCritters.data.hsqldb;
 
 import com.internetEnemies.combatCritters.data.IRegistry;
+import com.internetEnemies.combatCritters.data.hsqldb.DSOHelpers.BattleInfoHelper;
+import com.internetEnemies.combatCritters.data.hsqldb.DSOHelpers.CardHelper;
+import com.internetEnemies.combatCritters.objects.Card;
 import com.internetEnemies.combatCritters.objects.battles.Opponent;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,17 +29,61 @@ public class BattleInfoRegistryHSQLDB extends HSQLDBModel implements IRegistry<O
 
     @Override
     public Opponent getSingle(int id) {
-        return null;
+        try (Connection connection = this.connection()){
+            final PreparedStatement statement = connection.prepareStatement("SELECT * FROM BATTLEINFO WHERE id = ?");
+            statement.setInt(1, id);
+            final ResultSet resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return BattleInfoHelper.opponentFromResultSet(resultSet);
+            }
+            else {
+                return null;
+            }
+        }
+        catch (final SQLException e) {
+            throw new RuntimeException("An error occurred while processing the SQL operation", e);
+        }
     }
 
     @Override
     public List<Opponent> getListOf(List<Integer> ids) {
-        return null;
+        List<Opponent> opponents = new ArrayList<>();
+        try  (Connection connection = this.connection()){
+            StringBuilder queryBuilder = new StringBuilder("SELECT * FROM BATTLEINFO WHERE id IN (");
+            for (int i = 0; i < ids.size(); i++) {
+                queryBuilder.append("?");
+                if (i < ids.size() - 1) {
+                    queryBuilder.append(",");
+                }
+            }
+            queryBuilder.append(")");
+            final PreparedStatement statement = connection.prepareStatement(queryBuilder.toString());
+            for (int i = 0; i < ids.size(); i++) {
+                statement.setInt(i + 1, i); // Idk
+            }
+            final ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                opponents.add(BattleInfoHelper.opponentFromResultSet(resultSet));
+            }
+        } catch (final SQLException e) {
+            throw new RuntimeException("An error occurred while processing the SQL operation", e);
+        }
+        return opponents;
     }
 
     @Override
     public List<Opponent> getAll() {
-        return null;
+        List<Opponent> opponents = new ArrayList<>();
+        try  (Connection connection = this.connection()){
+            final PreparedStatement statement = connection.prepareStatement("SELECT * FROM BATTLEINFO");
+            final ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                opponents.add(BattleInfoHelper.opponentFromResultSet(resultSet));
+            }
+        } catch (final SQLException e) {
+            throw new RuntimeException("An error occurred while processing the SQL operation", e);
+        }
+        return opponents;
     }
 
     public Opponent addOpponent(){
