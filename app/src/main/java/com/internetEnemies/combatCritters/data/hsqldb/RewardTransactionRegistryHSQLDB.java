42e8273defd3fa0d5c885e002adfe57931dab5d5
Hugo Ng <ym7ym7228@gmail.com>
2024-03-27 14:58:56 -0500

    updated: RewardTransactionRegistryHSQLDB: implements IRegistry

--- app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/RewardTransactionRegistryHSQLDB.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/RewardTransactionRegistryHSQLDB.java
@@ -1,5 +1,10 @@
 package com.internetEnemies.combatCritters.data.hsqldb;
 
+import com.internetEnemies.combatCritters.data.IRegistry;
+import com.internetEnemies.combatCritters.objects.battles.RewardTransaction;
+
+import java.util.List;
+
 /**
  * RewardTransactionRegistryHSQLDB.java
  * COMP 3350 A02
@@ -9,5 +14,24 @@ package com.internetEnemies.combatCritters.data.hsqldb;
  * @PURPOSE     IRegistry implementation  for sql db of RewardTransaction
  */
 
-public class RewardTransactionRegistryHSQLDB {
+public class RewardTransactionRegistryHSQLDB extends HSQLDBModel implements IRegistry<RewardTransaction> {
+
+    public RewardTransactionRegistryHSQLDB(String dbPath) {
+        super(dbPath);
+    }
+
+    @Override
+    public RewardTransaction getSingle(int id) {
+        return null;
+    }
+
+    @Override
+    public List<RewardTransaction> getListOf(List<Integer> ids) {
+        return null;
+    }
+
+    @Override
+    public List<RewardTransaction> getAll() {
+        return null;
+    }
 }
