d876d9c13769f93b6fdc7346e35a832aacd5eed0
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-27 15:32:35 -0500

    updated: RewardTransactionRegistryHSQLDB: implement methods

--- app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/RewardTransactionRegistryHSQLDB.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/RewardTransactionRegistryHSQLDB.java
@@ -1,10 +1,20 @@
 package com.internetEnemies.combatCritters.data.hsqldb;
 
+import com.internetEnemies.combatCritters.Logic.TradeTransactionBuilder;
+import com.internetEnemies.combatCritters.Logic.battles.RewardTransactionBuilder;
 import com.internetEnemies.combatCritters.data.IRegistry;
+import com.internetEnemies.combatCritters.data.hsqldb.DSOHelpers.TransactionHelper;
 import com.internetEnemies.combatCritters.objects.ItemStack;
+import com.internetEnemies.combatCritters.objects.TradeTransaction;
+import com.internetEnemies.combatCritters.objects.battles.IRewardTransactionBuilder;
 import com.internetEnemies.combatCritters.objects.battles.RewardTransaction;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -25,17 +35,42 @@ public class RewardTransactionRegistryHSQLDB extends HSQLDBModel implements IReg
 
     @Override
     public RewardTransaction getSingle(int id) {
-        return null;
+        ResultSet rs = registry.getTransaction(TransactionRegistryHSQLDB.TYPE_REWARD, id);
+        RewardTransaction transaction;
+        try (Connection connection = this.connection()){
+            if(rs.next()) {
+                transaction = TransactionHelper.rewardFromResultSet(rs, connection);
+            } else {
+                transaction = null;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error getting a trade",e);
+        }
+        return transaction;
     }
 
     @Override
     public List<RewardTransaction> getListOf(List<Integer> ids) {
-        return null;
+        List<RewardTransaction> transactions = new ArrayList<>();
+        for (Integer id : ids) {
+            if(id == null) continue;
+            transactions.add(getSingle(id));
+        }
+        return transactions;
     }
 
     @Override
     public List<RewardTransaction> getAll() {
-        return null;
+        List<RewardTransaction> transactions = new ArrayList<>();
+        ResultSet rs = this.registry.getTransactions(TransactionRegistryHSQLDB.TYPE_REWARD);
+        try (Connection connection = this.connection()){
+            while(rs.next()) {
+                transactions.add(TransactionHelper.rewardFromResultSet(rs,connection));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Error getting a trade",e);
+        }
+        return transactions;
     }
 
     /**
@@ -43,7 +78,26 @@ public class RewardTransactionRegistryHSQLDB extends HSQLDBModel implements IReg
      * @param transaction RewardTransaction to add
      */
     public RewardTransaction add(RewardTransaction transaction){
-        return null;
+        IRewardTransactionBuilder builder = new RewardTransactionBuilder();
+        try(Connection connection = this.connection()){
+            //insert Transaction
+            PreparedStatement statement = connection.prepareStatement("INSERT INTO Transactions (type) VALUES (?)", Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, TransactionRegistryHSQLDB.TYPE_REWARD);
+            statement.executeUpdate();
+            ResultSet rs = statement.getGeneratedKeys();
+            rs.next();
+            int tid = rs.getInt(1);
+
+            //insert recieved
+            addStacks(tid, transaction.getReceived(), connection);
+
+            //rebuild
+            builder.fromTransaction(transaction);
+            builder.setID(tid);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error while adding a trade to the db",e);
+        }
+        return builder.build();
     }
 
     /**
