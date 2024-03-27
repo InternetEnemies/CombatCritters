3745789d1d054088b460480235dcf0aaf458bf25
whatdoes3plus1equalsto <ym7ym7228@gmail.com>
2024-03-27 15:24:15 -0500

    updated: RewardTransactionRegistryHSQLDB: add methods

--- app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/RewardTransactionRegistryHSQLDB.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/hsqldb/RewardTransactionRegistryHSQLDB.java
@@ -1,8 +1,10 @@
 package com.internetEnemies.combatCritters.data.hsqldb;
 
 import com.internetEnemies.combatCritters.data.IRegistry;
+import com.internetEnemies.combatCritters.objects.ItemStack;
 import com.internetEnemies.combatCritters.objects.battles.RewardTransaction;
 
+import java.sql.Connection;
 import java.util.List;
 
 /**
@@ -34,5 +36,25 @@ public class RewardTransactionRegistryHSQLDB extends HSQLDBModel implements IReg
     @Override
     public List<RewardTransaction> getAll() {
         return null;
+    }
+
+    /**
+     * add a transaction to the database, returns the actual transaction object related to the new transaction
+     * @param transaction RewardTransaction to add
+     */
+    public RewardTransaction add(RewardTransaction transaction){
+        return null;
+    }
+
+    /**
+     * add items to the TransactionItem Table
+     * @param tid id of the related transaction
+     * @param stacks item stacks to add
+     * @param connection connection to use
+     */
+    public void addStacks(int tid, List<ItemStack<?>> stacks, Connection connection) {
+        for (ItemStack<?> stack : stacks) {
+            stack.getItem().accept(new TransactionItemVisitor(tid, stack.getAmount(),true, connection));
+        }
     }
 }
