b91cbc94c53bcff614d2279069f877fcc6faeb6b
jackal <v@njackal.com>
2024-09-20 10:43:42 -0500

    integration tests passing

--- app/src/test/java/com/internetEnemies/combatCritters/TestUtils.java
+++ app/src/test/java/com/internetEnemies/combatCritters/TestUtils.java
@@ -5,13 +5,14 @@ import com.internetEnemies.combatCritters.application.Main;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class TestUtils {
     private static final File DB_SRC = new File("src/main/assets/db/DBInit.script");
 
     public static File copyDB() throws IOException {
         final File target = File.createTempFile("temp-db", ".script");
-        Files.copy(DB_SRC.toPath(), target.toPath());
+        Files.copy(DB_SRC.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
         Main.setDBPathName(target.getAbsolutePath().replace(".script", ""));
         return target;
     }
