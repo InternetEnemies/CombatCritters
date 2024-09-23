d9a5d8de3ed2bc63045b9e223b63e12d40b688ba
jackal <v@njackal.com>
2024-09-23 18:44:40 -0500

    spring initial setup

---
+++ critterSpring/src/main/java/com/combatcritters/critterspring/CritterSpringApplication.java
@@ -0,0 +1,13 @@
+package com.combatcritters.critterspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CritterSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CritterSpringApplication.class, args);
+	}
+
+}
