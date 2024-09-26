832f18694f04ace9395a96b789121e4e21c21d9c
jackal <v@njackal.com>
2024-09-26 12:04:38 -0500

    implemented and tested UserManager

--- app/src/main/java/com/internetEnemies/combatCritters/Logic/users/UserManager.java
+++ app/src/main/java/com/internetEnemies/combatCritters/Logic/users/UserManager.java
@@ -2,24 +2,27 @@ package com.internetEnemies.combatCritters.Logic.users;
 
 import com.internetEnemies.combatCritters.Logic.exceptions.UserNotFoundException;
 import com.internetEnemies.combatCritters.Logic.exceptions.UsernameConflictException;
+import com.internetEnemies.combatCritters.data.users.IUsersDB;
 import com.internetEnemies.combatCritters.objects.User;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-
-public class UserManager implements IUserManager{ //todo implement this properly
-    private final Dictionary<String, User> users;
-    public UserManager() {
-        this.users = new Hashtable<String, User>();
+public class UserManager implements IUserManager{ 
+    private final IUsersDB users;
+    public UserManager(IUsersDB users) {
+        this.users = users;
     }
     @Override
     public User getUserById(int id) {
-        return null;
+        User user = users.getUserById(id);
+        if(user == null) {
+            throw new UserNotFoundException("user with the given id was not found");
+        }
+        
+        return user;
     }
 
     @Override
     public User getUserByUsername(String username) {
-        User user = users.get(username);
+        User user = users.getUserByUsername(username);
         if(user == null) {
             throw new UserNotFoundException("user '"+username+"' not found");
         }
@@ -28,17 +31,16 @@ public class UserManager implements IUserManager{ //todo implement this properly
 
     @Override
     public boolean existsByUsername(String username) {
-        return users.get(username) != null;
+        return users.getUserByUsername(username) != null;
     }
 
     @Override
     public User createUser(String username, String password) {
-        User user = users.get(username);
+        User user = users.getUserByUsername(username);
         if(user != null) {
             throw new UsernameConflictException("user '"+username+"' already exists");
         }
-        user = new User(0,username,password);
-        users.put(username,user);
+        user = users.createUser(username, password);
         
         return user;
     }
