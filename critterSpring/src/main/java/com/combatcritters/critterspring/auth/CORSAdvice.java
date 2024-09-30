cf2f7c8329a9bd7f96d9b9266d68981079f2f037
jackal <v@njackal.com>
2024-09-30 18:08:39 -0500

    HEADERS ARE NOW GETTING SET

---
+++ critterSpring/src/main/java/com/combatcritters/critterspring/auth/CORSAdvice.java
@@ -0,0 +1,29 @@
+package com.combatcritters.critterspring.auth;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
+
+@ControllerAdvice
+public class CORSAdvice implements ResponseBodyAdvice<Object> {
+    @Override
+    public boolean supports(MethodParameter returnType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
+        if (body instanceof ResponseEntity) {
+            ResponseEntity responseEntity = (ResponseEntity) body;
+            responseEntity.getHeaders().add("Access-Control-Allow-Origin", "*");
+            System.out.println("FIRED");
+        }
+        response.getHeaders().add("Access-Control-Allow-Origin", "*");
+        return body;
+    }
+}
