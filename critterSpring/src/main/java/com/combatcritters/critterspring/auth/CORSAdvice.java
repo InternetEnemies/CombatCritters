784d2b8596b2da1751edbb9a0b49495e65dc1bc1
jackal <v@njackal.com>
2024-09-30 18:22:46 -0500

    cleanup and docs

--- critterSpring/src/main/java/com/combatcritters/critterspring/auth/CORSAdvice.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/auth/CORSAdvice.java
@@ -2,7 +2,6 @@ package com.combatcritters.critterspring.auth;
 
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
@@ -16,13 +15,22 @@ public class CORSAdvice implements ResponseBodyAdvice<Object> {
         return true;
     }
 
+    /*
+    A note to anyone looking at this code, I spent 8+ hours trying to resolve spring not sending cors headers, several of
+    which were spent trying to get spring to set them statically for all responses (as it turns out @RestControllers can't 
+    use Interceptors which is several of the top stack overflow results). I can guarantee that if you search how to setup
+    cors in spring you will find things that look like they should work (including the spring docs) and are better solutions than
+    this. I probably tried them, and they didn't work for me. Don't waste time here.
+    - jkl
+     */
+    /**
+     * adds static headers to all (non error) responses
+     */
     @Override
-    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class<? extends HttpMessageConverter<?>> selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (body instanceof ResponseEntity) {
-            ResponseEntity responseEntity = (ResponseEntity) body;
-            responseEntity.getHeaders().add("Access-Control-Allow-Origin", "*");
-            System.out.println("FIRED");
-        }
+    public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, 
+                                  Class<? extends HttpMessageConverter<?>> selectedConverterType, 
+                                  ServerHttpRequest request, ServerHttpResponse response) {
+        
         response.getHeaders().add("Access-Control-Allow-Origin", "*");
         return body;
     }
