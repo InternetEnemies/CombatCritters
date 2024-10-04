ab1c28b4bba2212e96aaf14515f9a2bd8fa5063c
jackal <v@njackal.com>
2024-10-04 13:46:00 -0500

    made changes for running from jar

--- critterSpring/src/main/java/com/combatcritters/critterspring/CritterSpringApplication.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/CritterSpringApplication.java
@@ -3,13 +3,14 @@ package com.combatcritters.critterspring;
 import com.internetEnemies.combatCritters.application.Main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.util.ResourceUtils;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
@@ -28,9 +29,9 @@ public class CritterSpringApplication {
 
 	public static void main(String[] args) throws IOException {
 		//!!! THE FOLLOWING CODE IS TEMPORARY AND WILL BE CHANGED SEE CombatCritters#51 FOR DETAILS !!!
-		final File DB_SRC = ResourceUtils.getFile("classpath:DBInit.script");
+		final InputStream DB_SRC = new ClassPathResource("DBInit.script").getInputStream();
 		final File target = File.createTempFile("temp-db", ".script");
-		Files.copy(DB_SRC.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+		Files.copy(DB_SRC, target.toPath(), StandardCopyOption.REPLACE_EXISTING);
 		Main.setDBPathName(target.getAbsolutePath().replace(".script", ""));
 		//!!! END TEMP !!!
 		SpringApplication.run(CritterSpringApplication.class, args);
