8450abfd073386ba166f13d68c0c4e7924eba6be
jackal <v@njackal.com>
2024-10-09 08:33:54 -0500

    converted UserProfile to record

--- app/src/main/java/com/internetEnemies/combatCritters/objects/UserProfile.java
+++ app/src/main/java/com/internetEnemies/combatCritters/objects/UserProfile.java
@@ -1,21 +1,14 @@
 package com.internetEnemies.combatCritters.objects;
 
-import java.util.Objects;
-
 /**
  * UserProfile.java
  * COMP 4350
- * @Project     Combat Critters 2.0
- * @created     10/9/24
- * 
- * @PURPOSE:    represent a users profile
+ *
+ * @Project Combat Critters 2.0
+ * @created 10/9/24
+ * @PURPOSE: represent a users profile
  */
-public class UserProfile {
-    private final int deckId;
-
-    public UserProfile(int deckId) {
-        this.deckId = deckId;
-    }
+public record UserProfile(int deckId) {
 
     @Override
     public boolean equals(Object o) {
@@ -25,12 +18,4 @@ public class UserProfile {
         return deckId == that.deckId;
     }
 
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(deckId);
-    }
-
-    public int getDeckId() {
-        return deckId;
-    }
 }
