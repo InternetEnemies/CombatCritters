ff0e6a1b5f1134e17ee979d98fca3ea4795c5854
VanJackal <v@njackal.com>
2024-10-14 13:01:37 -0500

    ping test

--- critterSpring/src/test/java/com/combatcritters/critterspring/CritterSpringApplicationTests.java
+++ critterSpring/src/test/java/com/combatcritters/critterspring/CritterSpringApplicationTests.java
@@ -1,13 +1,14 @@
 package com.combatcritters.critterspring;
 
-import com.combatcritters.critterspring.auth.payloads.LoginPayload;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.security.Principal;
+
 import static org.springframework.test.web.servlet.MockMvcBuilder.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -16,20 +17,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 @SpringBootTest
 class CritterSpringApplicationTests {
 	
-	MockHttpSession session;
 	private MockMvc mockMvc;
+	private Principal user;
 	
 	@BeforeEach
-	void setUp() throws Exception {
-		ObjectMapper mapper = new ObjectMapper();
+	void setUp() {
 		mockMvc = MockMvcBuilders.standaloneSetup(new CritterSpringApplication()).build();
-		session = new MockHttpSession();
-		mockMvc.perform(post("/users/auth/login").session(session).content(mapper.writeValueAsString(new LoginPayload("test","test"))));
+		user = new DummyPrincipal("user");
 	}
 
 	@Test
 	void test_ping() throws Exception {
-		mockMvc.perform(get("/ping").session(session));
+		mockMvc.perform(get("/ping").principal(user));
 	}
 
 }
