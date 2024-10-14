27d5db70ed71c6805a4d448fe0feb99c9ae731b4
VanJackal <v@njackal.com>
2024-10-14 17:58:20 -0500

    really basic cards test

--- critterSpring/src/test/java/com/combatcritters/critterspring/TestUtils.java
+++ critterSpring/src/test/java/com/combatcritters/critterspring/TestUtils.java
@@ -2,9 +2,17 @@ package com.combatcritters.critterspring;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.internetEnemies.combatCritters.objects.Card;
+import com.internetEnemies.combatCritters.objects.CritterCard;
+
+import java.util.List;
 
 public class TestUtils {
     public static String toJson(Object object) throws JsonProcessingException {
         return new ObjectMapper().writeValueAsString(object);
+    }
+    
+    public static CritterCard getDummyCritter(int id){
+        return new CritterCard(id, "name", "image", 0, Card.Rarity.COMMON,1,1, List.of());
     }
 }
