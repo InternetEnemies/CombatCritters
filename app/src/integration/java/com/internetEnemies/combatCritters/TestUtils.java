62b6c0f72720ae21b41b141a883e6cc8584b94a1
jackal <v@njackal.com>
2024-10-17 11:58:00 -0500

    moved to test containers

--- app/src/integration/java/com/internetEnemies/combatCritters/TestUtils.java
+++ app/src/integration/java/com/internetEnemies/combatCritters/TestUtils.java
@@ -5,22 +5,38 @@ import com.internetEnemies.combatCritters.application.Main;
 import com.internetEnemies.combatCritters.data.init.SQLInitializer;
 import com.internetEnemies.combatCritters.data.users.UsersDB;
 import com.internetEnemies.combatCritters.objects.User;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.io.IOException;
 
+/**
+ * this class using some hacky stuff to provide database to all test files, it's a bit weird, I'm not sorry.
+ */
 public class TestUtils {
     private static final String DUMMY_USER = "user";
     private static final String DUMMY_PASS = "pass";
+    public static PostgreSQLContainer<?> container;
 
     public static String getDBPath() throws IOException {
-        String path = "jdbc:postgresql://localhost:5432/critter_db?user=admin&password=admin";
+        resetDB();
+        String path = container.getJdbcUrl();
         Main.setDBPathName(path);
         SQLInitializer initializer = new SQLInitializer(path);
-        if(!initializer.isDbInitialized()){//todo remove this and switch to the container dbs
+        if(!initializer.isDbInitialized()){// it should never be initialized, but it'll error out if it somehow is
             initializer.initTables();
         }
         
         return path;
+    }
+    
+    public static void resetDB() {
+        if (container != null) {
+            container.stop();
+        }
+        container = new PostgreSQLContainer<>("postgres:13-alpine");
+        container.start();
+        container.withUrlParam("user", container.getUsername());
+        container.withUrlParam("password", container.getPassword());
     }
     
     public static User getDummyUser(String path) {
