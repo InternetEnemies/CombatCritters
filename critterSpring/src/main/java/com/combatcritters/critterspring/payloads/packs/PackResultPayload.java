cda10abe67f5867a4a475e8006e6d090358e2264
jackal <v@njackal.com>
2024-10-25 15:05:11 -0500

    packs routes implementation

--- critterSpring/src/main/java/com/combatcritters/critterspring/payloads/packs/PackResultPayload.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/payloads/packs/PackResultPayload.java
@@ -3,7 +3,6 @@ package com.combatcritters.critterspring.payloads.packs;
 import com.combatcritters.critterspring.payloads.CardPayload;
 import com.internetEnemies.combatCritters.objects.Card;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,10 +15,6 @@ import java.util.List;
  */
 public record PackResultPayload(List<CardPayload<?>> cards) {
     public static PackResultPayload from(List<Card> cards) {
-        List<CardPayload<?>> cardPayloads = new ArrayList<>();
-        for (Card card : cards) {
-            cardPayloads.add(CardPayload.from(card));
-        }
-        return new PackResultPayload(cardPayloads);
+        return new PackResultPayload(CardPayload.fromList(cards));
     }
 }
--- critterSpring/src/main/java/com/combatcritters/critterspring/routes/PacksController.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/routes/PacksController.java
@@ -4,6 +4,14 @@ import com.combatcritters.critterspring.payloads.CardPayload;
 import com.combatcritters.critterspring.payloads.ItemStackPayload;
 import com.combatcritters.critterspring.payloads.packs.PackResultPayload;
 import com.combatcritters.critterspring.payloads.packs.PackPayload;
+import com.internetEnemies.combatCritters.Logic.IUserDataFactory;
+import com.internetEnemies.combatCritters.Logic.inventory.packs.IPackCatalog;
+import com.internetEnemies.combatCritters.Logic.inventory.packs.IPackInventoryManager;
+import com.internetEnemies.combatCritters.Logic.users.IUserManager;
+import com.internetEnemies.combatCritters.objects.ItemStack;
+import com.internetEnemies.combatCritters.objects.Pack;
+import com.internetEnemies.combatCritters.objects.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,28 +22,52 @@ import java.util.List;
 @RestController
 public class PacksController {
     
+    private final IUserDataFactory userDataFactory;
+    private final IPackCatalog packCatalog;
+    private final IUserManager userManager;
+    
+    @Autowired
+    public PacksController(IUserDataFactory userDataFactory, IPackCatalog packCatalog, IUserManager userManager) {
+        this.userDataFactory = userDataFactory;
+        this.packCatalog = packCatalog;
+        this.userManager = userManager;
+    }
+    
+    
     @GetMapping("/packs")
     public List<PackPayload> getPacks() {
-        throw new RuntimeException("not implemented");//todo
+        List<Pack> packs = packCatalog.getListOfPacks();
+        return packs.stream().map(PackPayload::from).toList();
     }
     
     @GetMapping("/packs/{packid}")
     public PackPayload getPack(@PathVariable int packid) {
-        throw new RuntimeException("not implemented");//todo
+        Pack pack = packCatalog.getPack(packid);
+        return PackPayload.from(pack);
     }
     
     @GetMapping("/users/{userid}/packs")
-    public ItemStackPayload<PackPayload> getPacks(@PathVariable int userid) {
-        throw new RuntimeException("not implemented");//todo
+    public List<ItemStackPayload<PackPayload>> getPacks(@PathVariable int userid) {
+        User user = this.userManager.getUserById(userid);
+        IPackInventoryManager packInv = this.userDataFactory.getPackInventoryManger(user);
+        List<ItemStack<Pack>> packs = packInv.getPackCounts();
+        return packs.stream().map(pack -> new ItemStackPayload<>(
+                pack.getAmount(),
+                PackPayload.from(pack.getItem()
+                ))).toList();
     }
     
     @GetMapping("/packs/{packid}/contents")
     public List<CardPayload<?>> getPackContents(@PathVariable int packid) {
-        throw new RuntimeException("not implemented");//todo
+        Pack pack = packCatalog.getPack(packid);
+        return CardPayload.fromList(pack.getSetList());
     }
     
     @PostMapping("/users/{userid}/packs/{packid}")
     public PackResultPayload openPack(@PathVariable int userid, @PathVariable int packid) {
-        throw new RuntimeException("not implemented");//todo
+        User user = this.userManager.getUserById(userid);
+        Pack pack = this.packCatalog.getPack(packid);
+        IPackInventoryManager packInv = this.userDataFactory.getPackInventoryManger(user);
+        return PackResultPayload.from(packInv.openPack(pack));
     }
 }
