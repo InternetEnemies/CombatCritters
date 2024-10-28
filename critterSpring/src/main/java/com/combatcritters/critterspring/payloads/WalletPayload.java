ed8c329f1db154e9f5c782414b3749c08dfc5b69
jackal <v@njackal.com>
2024-10-28 11:14:06 -0500

    docs

--- critterSpring/src/main/java/com/combatcritters/critterspring/payloads/WalletPayload.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/payloads/WalletPayload.java
@@ -2,6 +2,14 @@ package com.combatcritters.critterspring.payloads;
 
 import com.internetEnemies.combatCritters.objects.Currency;
 
+/**
+ * WalletPayload.java
+ * COMP 4350
+ * @Project     Combat Critters 2.0
+ * @created     10/28/24
+ * 
+ * @PURPOSE:    payload for a users wallet
+ */
 public record WalletPayload(int coins) {
     public static WalletPayload from(Currency currency) {
         return new WalletPayload(currency.getAmount());
