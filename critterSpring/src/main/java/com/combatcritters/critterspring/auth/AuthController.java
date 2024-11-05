0a924c800792a532b5a34769005a7b43b326cf1c
jackal <v@njackal.com>
2024-11-05 10:38:11 -0600

    added get current user route

--- critterSpring/src/main/java/com/combatcritters/critterspring/auth/AuthController.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/auth/AuthController.java
@@ -16,11 +16,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.security.Principal;
 
 /**
  * AuthController.java
@@ -83,5 +82,11 @@ public class AuthController {
                 passwordEncoder.encode(payload.password())
         );
         return new ResponseEntity<>("Registered new user:"+user.getUsername(), HttpStatus.CREATED);
+    }
+    
+    @GetMapping("/user")
+    public UserPayload getCurrentUser(Principal principal){
+        User user = userManager.getUserByUsername(principal.getName());
+        return UserPayload.from(user);
     }
 }
