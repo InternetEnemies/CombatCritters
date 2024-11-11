c9409e9816567d4a4f6b2426c3b32616dbfef29d
jackal <v@njackal.com>
2024-11-10 22:00:14 -0600

    discounts db sans integration tests

--- app/src/main/java/com/internetEnemies/combatCritters/data/market/VendorOfferDB.java
+++ app/src/main/java/com/internetEnemies/combatCritters/data/market/VendorOfferDB.java
@@ -6,11 +6,17 @@ import com.internetEnemies.combatCritters.data.hsqldb.HSQLDBUserModel;
 import com.internetEnemies.combatCritters.data.hsqldb.dataHandlers.GenericSQLOperations;
 import com.internetEnemies.combatCritters.data.hsqldb.sqlHelpers.ResultListExtractor;
 import com.internetEnemies.combatCritters.data.hsqldb.sqlHelpers.SingleResultExtractor;
+import com.internetEnemies.combatCritters.data.hsqldb.transactions.DiscountTransactionDetails;
+import com.internetEnemies.combatCritters.data.hsqldb.transactions.DiscountTransactionDetailsHelper;
+import com.internetEnemies.combatCritters.objects.DiscountTransaction;
 import com.internetEnemies.combatCritters.objects.User;
 import com.internetEnemies.combatCritters.objects.VendorTransaction;
 import com.internetEnemies.combatCritters.objects.market.VendorDetails;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 public class VendorOfferDB extends HSQLDBUserModel implements IVendorOfferDB {
     private final VendorDetails vendorDetails;
@@ -50,9 +56,47 @@ public class VendorOfferDB extends HSQLDBUserModel implements IVendorOfferDB {
                 "Error getting vendor special offers"
         );
     }
-    
+
+    @Override
+    public List<DiscountTransaction> getDiscountOffers() {
+        //? This is kinda inefficient, it may need to be improved at some point. main issue is the loop over an sql call
+        //? the current design is intended to remain as O(n), a potential improvement would involve a more specialized result handler 
+        //Get Discount objects
+        List<DiscountTransactionDetails> discountTransactions = execute(
+                GenericSQLOperations.query(ResultListExtractor.getListExtractor(DiscountTransactionDetailsHelper::fromResultSet)),
+                VendorOfferSQL.getVendorDiscounts(vendorDetails.id(),getLevel()),
+                "Error getting discount offers"
+        );
+        List<Integer> discountIds = new LinkedList<>();
+        List<Integer> parentIds = new LinkedList<>();
+        for(DiscountTransactionDetails discountTransaction : discountTransactions) {
+            discountIds.add(discountTransaction.tid());
+            parentIds.add(discountTransaction.parent());
+        }
+        //Get Parent Transactions
+        Map<Integer, VendorTransaction> parents = getOffersList(parentIds);
+        //Get Discount Transaction
+        Map<Integer, VendorTransaction> discounts = getOffersList(discountIds);
+        
+        return discountTransactions.stream().map(details -> new DiscountTransaction(
+                parents.get(details.parent()),
+                discounts.get(details.discount()),
+                details.discount())).toList();
+    }// maybe figuring out the single sql would have been easier, whatever lol
+
     private int getLevel() {
         IVendorRepManager repManager = vendorRepManagerFactory.create(getUser(), vendorDetails);
         return repManager.getVendorRep().level();
+    }
+    
+    private Map<Integer, VendorTransaction> getOffersList(List<Integer> offerIds) {
+        Map<Integer, VendorTransaction> offersMap = new HashMap<>();
+        execute(
+                GenericSQLOperations.query(ResultListExtractor.getListExtractor(vendorOfferHelper::fromResultSet)),
+                VendorOfferSQL.getVendorOffersFromIds(vendorDetails.id(), getLevel(), offerIds),
+                "Error getting vendor offer list"
+        )
+                .forEach(offer -> offersMap.put(offer.getId(), getVendorOffer(offer.getId())));
+        return offersMap;
     }
 }
