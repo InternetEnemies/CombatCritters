65ca108256bf22a3a68a9178a99082721a0d2212
VanJackal <v@njackal.com>
2024-12-05 18:57:12 -0600

    removed undocumented route that contained xss

--- critterSpring/src/main/java/com/combatcritters/critterspring/CritterSpringApplication.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/CritterSpringApplication.java
@@ -2,11 +2,8 @@ package com.combatcritters.critterspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
 
 /**
  * CritterSpringApplication.java
@@ -22,11 +19,6 @@ public class CritterSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CritterSpringApplication.class, args);
-	}
-	
-	@GetMapping("/ping")
-	public String ping(Principal principal, @RequestParam(value = "reply", defaultValue = "pong") String reply) {
-		return reply + "\n" + principal.getName();
 	}
 
 }
