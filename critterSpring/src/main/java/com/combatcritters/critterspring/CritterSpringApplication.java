4da76695c58701d9eedba83ff9aff87504b216c9
VanJackal <V@njackal.com>
2024-12-11 18:42:02 -0600

    Revert "removed undocumented route that contained xss"

--- critterSpring/src/main/java/com/combatcritters/critterspring/CritterSpringApplication.java
+++ critterSpring/src/main/java/com/combatcritters/critterspring/CritterSpringApplication.java
@@ -2,8 +2,11 @@ package com.combatcritters.critterspring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 
 /**
  * CritterSpringApplication.java
@@ -19,6 +22,11 @@ public class CritterSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CritterSpringApplication.class, args);
+	}
+	
+	@GetMapping("/ping")
+	public String ping(Principal principal, @RequestParam(value = "reply", defaultValue = "pong") String reply) {
+		return reply + "\n" + principal.getName();
 	}
 
 }
